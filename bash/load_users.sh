c34048a5516b9d5fe686f41d7cb0e98762747125
Ubuntu <ubuntu@ip-172-31-10-249.eu-central-1.compute.internal>
2019-10-17 20:36:03 +0000

    Do file globbing for load.

--- bash/load_users.sh
+++ bash/load_users.sh
@@ -3,17 +3,28 @@
 UFILE=$1;
 if [ -z $UFILE ]
   then
-    echo 'Usage: ./load.sh <users_file>'
+    echo 'Usage: ./load.sh <users_file(s)>'
     exit
-fi
-if [ ! -f $UFILE ]
-  then
-    echo 'Users file not found'
-    exit;
 fi
 
 TMP='./.okapi'
 OKAPI=`cat ${TMP}/url`
 TOKEN=`cat ${TMP}/token`
 
-curl --http1.1 "${OKAPI}/user-import" -H 'content-type: application/json' -H "x-okapi-token: ${TOKEN}" -d @$UFILE
+if [ ! -d '/data/folio-data/done' ]
+  then
+    mkdir '/data/folio-data/done'
+fi
+
+if [ ! -d 'log' ]
+  then
+    mkdir 'log'
+fi
+
+for f in ${BASH_ARGV[*]}; do
+  echo "Loading ${f}"
+  echo '' >> 'log/users.log'
+  echo $f >> 'log/users.log'
+  curl --http1.1 "${OKAPI}/user-import" -H 'content-type: application/json' -H "x-okapi-token: ${TOKEN}" -d @$f >> 'log/users.log'
+  mv $f '/data/folio-data/done'
+done
