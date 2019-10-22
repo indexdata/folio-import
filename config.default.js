5094cb030b28ab29f9c479e55d385169e1fea633
David Crossley <david@indexdata.com>
2019-10-22 14:37:37 +1100

    Fix typo in default configuration

--- config.default.js
+++ config.default.js
@@ -5,6 +5,6 @@ module.exports = {
   username: 'diku_admin',
   password: 'admin',
   delay: 500, // delay between record loads in ms
-  ocationId: 'fcd64ce1-6995-48f0-840e-89ffa2288371', // optional default location ID for holdings records.
+  locationId: 'fcd64ce1-6995-48f0-840e-89ffa2288371', // optional default location ID for holdings records.
   loanTypeId: '2b94c631-fca9-4892-a730-03ee529ffe27' // optional default loan type ID for item records
 };
