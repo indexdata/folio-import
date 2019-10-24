5470cd3732a2c3227cafd2ff019569c9d014a7e4
Charles Ledvina <charles@indexdata.com>
2019-10-24 10:20:08 -0500

    Script for deleting instance formats.

---
+++ deleteInstanceFormats.js
@@ -0,0 +1,54 @@
+/*
+  Delete instance formats by instanceFormats reference collection file.
+*/
+
+const fs = require('fs');
+const superagent = require('superagent');
+const { getAuthToken } = require('./lib/login');
+let inFile = process.argv[2];
+
+const wait = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+(async () => {
+  try {
+    let inData;
+    if (!inFile) {
+      throw new Error('Usage: node deleteInstanceFormats.js <instance_formats_file>');
+    } else if (!fs.existsSync(inFile)) {
+      throw new Error('Can\'t find input file');
+    } else {
+      try {
+        inData = require(inFile);
+      } catch (e) {
+        inData = require(`./${inFile}`); 
+      }
+      if (inData.instanceFormats) {
+        inData = inData.instanceFormats;
+      }
+    }
+    const config = (fs.existsSync('./config.js')) ? require('./config.js') : require('./config.default.js');
+
+    const authToken = await getAuthToken(superagent, config.okapi, config.tenant, config.authpath, config.username, config.password);
+
+    const actionUrl = config.okapi + '/instance-formats';
+
+    for (let x = 0; x < inData.length; x++) {
+      let id = inData[x].id;
+      console.log(`Deleting ${id}`);
+      try {
+        await superagent
+          .delete(`${actionUrl}/${id}`)
+          .set('accept', 'text/plain')
+          .set('x-okapi-tenant', config.tenant)
+          .set('x-okapi-token', authToken);
+      } catch (e) {
+        console.error(e.response.text);
+      }
+      await wait(config.delay);
+    }
+  } catch (e) {
+    console.error(e.message);
+  }
+})();
