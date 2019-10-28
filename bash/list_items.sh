4612e4c534e00769a3ffec84c61d443e64b2eeda
Charles Ledvina <charles@indexdata.com>
2019-10-28 17:09:40 -0500

    Add query argument

--- bash/list_items.sh
+++ bash/list_items.sh
@@ -4,4 +4,9 @@ TMP='./.okapi'
 OKAPI=`cat ${TMP}/url`
 TOKEN=`cat ${TMP}/token`
 
-curl --http1.1 -w '\n' "${OKAPI}/item-storage/items" -H "x-okapi-token: ${TOKEN}"
+if [ $1 ] 
+  then
+  Q="?query=$1";
+fi
+
+curl --http1.1 -w '\n' "${OKAPI}/item-storage/items${Q}" -H "x-okapi-token: ${TOKEN}"
