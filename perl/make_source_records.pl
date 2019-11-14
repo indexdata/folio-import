930b9101696e25f57493d879b1a38fdd809575a4
Charles Ledvina <charles@indexdata.com>
2019-11-14 13:19:45 -0600

    Handle file globs.

--- perl/make_source_records.pl
+++ perl/make_source_records.pl
@@ -12,16 +12,6 @@ binmode STDOUT, ":utf8";
 $| = 1;
 
 my $ctrl_file = shift;
-my $infile = shift or die "Usage: ./marc_source_records.pl <uuid_map_file> <raw_marc_collection>\n";
-if (! -e $infile) {
-  die "Can't find raw Marc file!\n"
-}
-if (! -e $ctrl_file) {
-  die "Can't find id to uuid map file\n";
-}
-
-$save_path = $infile;
-$save_path =~ s/^(.+)\..+$/$1_srs.json/;
 
 sub uuid {
   my $ug = Data::UUID->new;
@@ -40,30 +30,47 @@ sub getIds {
 
 my $id_map = getIds();
 
-# open a collection of raw marc records
-my $srs_recs = { records=>[] };
-$/ = "\x1D";
-open RAW, "<:encoding(UTF-8)", $infile;
-while (<RAW>) {
-  my $raw = $_;
-  my $srs = {};
-  my $marc = MARC::Record->new_from_usmarc($raw);
-  my $mij = MARC::Record::MiJ->to_mij($marc);
-  my $parsed = decode_json($mij);
-  my $control_num = $marc->field('001')->{_data};
-  $srs->{id} = uuid();
-  my $nine = {};
-  $nine->{'999'} = { subfields=>[ { 'i'=>$id_map->{$control_num} }, { 's'=>$srs->{id} } ] };
-  $nine->{'999'}->{'ind1'} = 'f';
-  $nine->{'999'}->{'ind2'} = 'f';
-  push @{ $parsed->{fields} }, $nine;
-  $srs->{snapshotId} = 'TO BE ADDED BY LOADING SCRIPT';
-  $srs->{matchedId} = uuid();
-  $srs->{recordType} = 'MARC';
-  $srs->{rawRecord} = { id=>uuid(), content=>$raw };
-  $srs->{parsedRecord} = { id=>uuid(), content=>$parsed };
-  push $srs_recs->{records}, $srs;
+foreach (@ARGV) {
+  my $infile = $_ or die "Usage: ./marc_source_records.pl <uuid_map_file> <raw_marc_collection>\n";
+  if (! -e $infile) {
+    die "Can't find raw Marc file!\n"
+  }
+  if (! -e $ctrl_file) {
+    die "Can't find id to uuid map file\n";
+  }
+
+  my $save_path = $infile;
+  $save_path =~ s/^(.+)\..+$/$1_srs.json/;
+
+  # open a collection of raw marc records
+  my $srs_recs = { records=>[] };
+  $/ = "\x1D";
+  my $count = 0;
+  open RAW, "<:encoding(UTF-8)", $infile;
+  while (<RAW>) {
+    $count++;
+    print "\r$count";
+    my $raw = $_;
+    my $srs = {};
+    my $marc = MARC::Record->new_from_usmarc($raw);
+    my $mij = MARC::Record::MiJ->to_mij($marc);
+    my $parsed = decode_json($mij);
+    my $control_num = $marc->field('001')->{_data};
+    $srs->{id} = uuid();
+    my $nine = {};
+    $nine->{'999'} = { subfields=>[ { 'i'=>$id_map->{$control_num} }, { 's'=>$srs->{id} } ] };
+    $nine->{'999'}->{'ind1'} = 'f';
+    $nine->{'999'}->{'ind2'} = 'f';
+    push @{ $parsed->{fields} }, $nine;
+    $srs->{snapshotId} = 'TO BE ADDED BY LOADING SCRIPT';
+    $srs->{matchedId} = uuid();
+    $srs->{recordType} = 'MARC';
+    $srs->{rawRecord} = { id=>uuid(), content=>$raw };
+    $srs->{parsedRecord} = { id=>uuid(), content=>$parsed };
+    push $srs_recs->{records}, $srs;
+  }
+  $out = JSON->new->pretty->encode($srs_recs);
+  open OUT, ">:encoding(UTF-8)", $save_path;
+  print OUT $out;
+  print "\nDone! SRS records saved to $save_path\n";
 }
-$out = JSON->new->pretty->encode($srs_recs);
-open OUT, ">:encoding(UTF-8)", $save_path;
-print OUT $out;
