b211d804971eee3e04ad53c5fda7fbaf2736f644
Charles Ledvina <charles@indexdata.com>
2019-11-15 12:01:26 -0600

    Add query to list_holdings.sh

--- bash/list_holdings.sh
+++ bash/list_holdings.sh
@@ -4,4 +4,9 @@ TMP='./.okapi'
 OKAPI=`cat ${TMP}/url`
 TOKEN=`cat ${TMP}/token`
 
-curl --http1.1 -w '\n' "${OKAPI}/holdings-storage/holdings" -H "x-okapi-token: ${TOKEN}"
+if [ $1 ]
+  then
+  Q="?query=$1";
+fi
+
+curl --http1.1 -w '\n' "${OKAPI}/holdings-storage/holdings${Q}" -H "x-okapi-token: ${TOKEN}"
