02ece1c19fb2fc20e24d98ba30d036f178605396
Charles Ledvina <charles@indexdata.com>
2020-01-08 11:38:02 -0600

    Add scripts to deal with modes of issuance.

---
+++ deleteModesOfIssuance.js
@@ -0,0 +1,54 @@
+/*
+  Delete nature of contents by natureOfContentTerms reference collection file.
+*/
+
+const fs = require('fs');
+const superagent = require('superagent');
+const { getAuthToken } = require('./lib/login');
+let inFile = process.argv[2];
+
+const wait = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+(async () => {
+  try {
+    let inData;
+    if (!inFile) {
+      throw new Error('Usage: node deleteModesOfIssuance.js <modes_of_issuance_file>');
+    } else if (!fs.existsSync(inFile)) {
+      throw new Error('Can\'t find input file');
+    } else {
+      try {
+        inData = require(inFile);
+      } catch (e) {
+        inData = require(`./${inFile}`); 
+      }
+      if (inData.issuanceModes) {
+        inData = inData.issuanceModes;
+      }
+    }
+    const config = (fs.existsSync('./config.js')) ? require('./config.js') : require('./config.default.js');
+
+    const authToken = await getAuthToken(superagent, config.okapi, config.tenant, config.authpath, config.username, config.password);
+
+    const actionUrl = config.okapi + '/modes-of-issuance';
+
+    for (let x = 0; x < inData.length; x++) {
+      let id = inData[x].id;
+      console.log(`Deleting ${id}`);
+      try {
+        await superagent
+          .delete(`${actionUrl}/${id}`)
+          .set('accept', 'text/plain')
+          .set('x-okapi-tenant', config.tenant)
+          .set('x-okapi-token', authToken);
+      } catch (e) {
+        console.error(e.response.text);
+      }
+      await wait(config.delay);
+    }
+  } catch (e) {
+    console.error(e.message);
+  }
+})();
