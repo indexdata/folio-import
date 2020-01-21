bb99322f273a9a8f12d5712adf5a97e989d52a0b
Charles Ledvina <charles@indexdata.com>
2020-01-21 11:05:34 -0500

    Make control number configurabl.

--- perl/marc_inst_diff.pl
+++ perl/marc_inst_diff.pl
@@ -7,7 +7,8 @@ use JSON;
 binmode STDOUT, ":utf8";
 
 my $infile = shift;
-my $keyfile = shift or die "Usage: ./marc_inst_diff.pl <raw_marc_file> <inst_file>\n";
+my $keyfile = shift or die "Usage: ./marc_inst_diff.pl <raw_marc_file> <inst_file> [<record_number_field>]";
+my $field = shift || '001';
 if (! -e $infile) {
   die "Can't find marc file!\n"
 }
@@ -28,18 +29,20 @@ $outfile =~ s/(.+)\.\w+$/$1/;
 $outfile = "${outfile}_not_loaded.mrc";
 unlink $outfile;
 open OUT, ">>:utf8", $outfile or die "Can't open outfile!";
+my $num = 0;
 while (<RAW>) {
+  $num++;
   $raw = $_;
   my $marc = MARC::Record->new_from_usmarc($raw);
-  my $bnum = $marc->subfield('907',"a");
-  unless ($ids->{$bnum}) {
-    print Dumper("--------------");
-    foreach ($marc->field('020')) {
-      my $f = $_;
-      foreach ($f->subfields('a')) {
-        print Dumper($_);
-      }
-    }
+  my $cnum;
+  if ($field =~ /^00/) {
+    $cnum = $marc->field($field)->as_string();
+  } else {
+    $field =~ /^(...)(.)/;
+    $cnum = $marc->subfield($1, $2);
+  }
+  unless ($ids->{$cnum}) {
+    print "Rec # $num ($cnum) not found\n";
     print OUT $marc->as_usmarc();
   }
 }
