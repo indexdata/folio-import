88fb566d732c7785fc1fe8e37ec584a13360ce36
Charles Ledvina <charles@indexdata.com>
2020-01-21 12:21:51 -0500

    Move repeated 020 $a to new 020 fields.

--- perl/marc_fix.pl
+++ perl/marc_fix.pl
@@ -22,11 +22,19 @@ while (<RAW>) {
   $raw = $_;
   my $marc = MARC::Record->new_from_usmarc($raw);
   foreach ($marc->field('020')) {
-    print "----------------\n";
     my $f = $_;
-    @suba = $f->subfield('a');
-    $alen = @suba;
-    print "$alen\n";
+    my $fc = 0;
+    foreach ($f->subfield('a')) {
+      $data = $_;
+      if ($fc > 0) {
+        my $field = MARC::Field->new('020', ' ', ' ', 'a' => $data);
+        $marc->insert_fields_after($f, $field);
+        $f->delete_subfield(code => 'a', pos => 1);
+      }
+      $fc++;
+    } 
   }
+  print $marc->as_formatted();
+  print "\n";
   # print OUT $marc->as_usmarc();
 }
