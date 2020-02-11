08a4419d9f80d295019b8bbcc2d50990cb09532d
Charles Ledvina <charles@indexdata.com>
2020-02-11 12:27:26 -0600

    A new utility for mapping static ids to holdings.

---
+++ util/staticHoldingsId.js
@@ -0,0 +1,45 @@
+/* 
+This script will grab ids from an existing holdingsRecords collection and match on formerIds.  
+This is for the purposes of keeping static ids in order to not mess-up any service that is
+depending on them (i.e. course reserves, vufind).
+
+This script will take first a collection of existing holdings records and then the new collection
+as inputs.
+*/
+
+const fs = require('fs');
+
+const staticRecs = process.argv[2];
+const newRecs = process.argv[3];
+
+if (! (staticRecs && newRecs)) {
+  console.log('Usage: node staticHoldingsId.js <static_collection> <new_collection> [ <match field>,<match field>,... ]');
+}
+
+const static = require(staticRecs);
+const updated = require(newRecs);
+let mkeys = process.argv[4].split(/,/) || [ formerIds ];
+const staticId = {};
+static.holdingsRecords.forEach(s => {
+  let vals = [];
+  mkeys.forEach(k => {
+    vals.push(s[k]);
+  });
+  let matchKey = vals.join(':');
+  staticId[matchKey] = s.id;
+});
+
+updated.holdingsRecords.forEach(u => {
+  let vals = [];
+  mkeys.forEach(k => {
+    vals.push(u[k]);
+  });
+  let matchKey = vals.join(':');
+  if (staticId[matchKey]) {
+    u.id = staticId[matchKey];
+  }
+});
+
+const outFile = newRecs.replace(/\.json$/, '_static.json');
+console.log(`Writing to ${outFile}`);
+fs.writeFileSync(outFile, JSON.stringify(updated, null, 2));
