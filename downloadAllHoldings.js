47c8f3bc0d8159bae0e047164e9a4c9b0f1f824b
Charles Ledvina <charles@indexdata.com>
2020-02-11 12:39:59 -0600

    New script for downloading all holdings records.

---
+++ downloadAllHoldings.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const superagent = require('superagent');
+const { getAuthToken } = require('./lib/login');
+let refDir = process.argv[2];
+let start = parseInt(process.argv[3], 10);
+let limit = parseInt(process.argv[4], 10);
+
+(async () => {
+  try {
+    if (!refDir) {
+      throw new Error('Usage: node downloadAllHoldings.js <download_dir> <start> <stop>');
+    } else if (!fs.existsSync(refDir)) {
+      throw new Error('Reference directory does\'t exist!');
+    } else if (!fs.lstatSync(refDir).isDirectory()) {
+      throw new Error(`${refDir} is not a directory!`)
+    }
+    const config = (fs.existsSync('./config.js')) ? require('./config.js') : require('./config.default.js');
+
+    const authToken = await getAuthToken(superagent, config.okapi, config.tenant, config.authpath, config.username, config.password);
+
+    refDir = refDir.replace(/\/$/,'');
+
+    const actionUrl = config.okapi + '/holdings-storage/holdings';
+
+    let totFetch = 0;
+    let totRecs = 1000000;
+    let perPage = 500;
+    let offset = start || 0;
+    const coll = { holdingsRecords: [] };
+    while (totFetch < totRecs) {
+      let url = `${actionUrl}?limit=${perPage}&offset=${offset}`;
+      try {
+        let res = await superagent
+          .get(url)
+          .timeout({response: 10000})
+          .set('accept', 'application/json')
+          .set('x-okapi-token', authToken);
+        coll.holdingsRecords = coll.holdingsRecords.concat(res.body.holdingsRecords);
+        totFetch = coll.holdingsRecords.length;
+        if (start) {
+          totFetch += start;
+        }
+        totRecs = limit || res.body.totalRecords;
+      } catch (e) {
+        try {
+          console.log(e.response.text);
+        } catch {
+          console.log(e.message);
+        }
+      }
+      offset += perPage;
+      console.log(url);
+      console.log(`Received ${totFetch} of ${totRecs}...`);
+    }
+    const fn = `${refDir}/holdings.json`
+    console.log(`Writing to ${fn}`);
+    const jsonStr = JSON.stringify(coll, null, 2);
+    fs.writeFileSync(fn, jsonStr);
+  } catch (e) {
+    console.error(e.message);
+  }
+})();
