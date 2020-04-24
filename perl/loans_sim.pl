ffd0222ebd76a45a6cad35880ad4e8dc0c15ddd9
Charles Ledvina <charles@indexdata.com>
2020-04-24 10:05:20 -0500

    Convert loandate to ISO.

--- perl/loans_sim.pl
+++ perl/loans_sim.pl
@@ -21,6 +21,7 @@ $path =~ s/^(.+)\/.+/$1/;
 
 my $sp = '3a40852d-49fd-4df2-a1f9-6e2641a6e91f';
 my $line = 0;
+my $coc = 0;
 my $checkout = { checkouts => [] };
 while (<TSV>) {
   chomp;
@@ -28,19 +29,35 @@ while (<TSV>) {
   $line++;
   next if $line == 1;
   my ($bc, $ldate, $due, $au, $ti, $pname, $userbc) = split /\t/;
-  my $co = {
-    itemBarcode => $bc,
-    userBarcode => $userbc,
-    servicePointId => $sp,
-    loadDate => $ldate
-  };
+  my $iso_ldate = date_conv($ldate);
   if ($bc && $userbc) {
-    push @{ $checkout->{checkouts} }, $co;
+    my $co = {
+      itemBarcode => $bc,
+      userBarcode => $userbc,
+      servicePointId => $sp,
+      loadDate => $iso_ldate
+    };
+    if ($bc && $userbc) {
+      push @{ $checkout->{checkouts} }, $co;
+    }
+    $coc++;
+  } else {
+    print "WARN [$line] There is a missing item or user barcode: item: $bc, user: $userbc\n"
   }
+  last if $line > $limit;
 }
 my $co_json = to_json($checkout, {utf8 => 1, pretty => 1});
-print $co_json;
+my $co_out = "$path/checkouts.json";
+print "Writing $coc checkout records to $co_out\n";
+open OUT, ">$co_out" or die "Can't write to $co_out\n!";
+print OUT $co_json;
 
+sub date_conv {
+  $in = shift;
+  my ($m, $d, $y, $h, $min) = $in =~ /^(\d+)\/(\d+)\/(\d+) (\d+):(\d)/;
+  my $out = sprintf('%04d-%02d-%02dT%02d:%02d:00.000', $y, $m, $d, $h, $min);
+  return $out;
+}
 
 sub uuid {
   my $ug = Data::UUID->new;
