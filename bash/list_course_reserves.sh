0ab3caf62ae95a6d4d702299359efd0e6f4da807
Charles Ledvina <charles@indexdata.com>
2020-05-07 13:33:01 -0500

    New configs

--- bash/list_course_reserves.sh
+++ bash/list_course_reserves.sh
@@ -4,4 +4,4 @@ TMP='./.okapi'
 OKAPI=`cat ${TMP}/url`
 TOKEN=`cat ${TMP}/token`
 
-curl --http1.1 -w '\n' "${OKAPI}/coursereserves/reserves" -H "x-okapi-token: ${TOKEN}"
+curl --http1.1 -w '\n' "${OKAPI}/coursereserves/reserves${1}" -H "x-okapi-token: ${TOKEN}"
