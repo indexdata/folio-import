ad393b56d6d2ee573bc84ea3140cc65ece4e6292
Charles Ledvina <charles@indexdata.com>
2020-05-20 16:58:16 -0500

    Create precedingSucceedingTitles object

--- perl/make_presuc.pl
+++ perl/make_presuc.pl
@@ -43,16 +43,19 @@ unlink $outfile;
 open OUT, ">>:utf8", $outfile or die "Can't open outfile!";
 my $count = 0;
 my $found = 0;
+print "Looking for matches...\n";
+my $out = { precedingSucceedingTitles => [] };
 while (<RAW>) {
   $count++;
   next if $count < $start;
-  print "Checking record $count\n";
+  # print "Checking record $count\n";
   $raw = $_;
-  my $pretitle;
   my $marc = MARC::Record->new_from_usmarc($raw);
   my $hrid = $marc->subfield('907', 'a');
-  if ($marc->field('780') || $marc->field('785')) {
+  if ($marc->field('780')) {
+    my $psObj = {};
     my @pre_ids;
+    my $done = 0;
     foreach ($marc->field('780')) {
       my $field = $_;
       foreach ($_->subfield('w')) {
@@ -62,16 +65,39 @@ while (<RAW>) {
         push @pre_ids, "(ISSN)$_";
       }
       foreach (@pre_ids) {
-        # print "$_\n";
         my $match = $bib_ids->{$_};
+        next if $match eq $hrid;
+        $match =~ s/^\.(b\d{7}).*/$1/;
+        $hrid =~ s/^\.(b\d{7}).*/$1/;
         if ($match) {
-          print "RECORD FOUND ($_ -> $match)\n";
-          $pretitle = $field->as_string('atgwxzor'); 
-          print "$hrid $pretitle\n";
+          print "$hrid\n";
+          print "$_\n";
+          $found++;
+          my $inst_id = $hrid2inst->{$match};
+          # print "[$found] RECORD FOUND ($_ -> $match -> $inst_id)\n";
+          my $pretitle = $field->as_string('atg');
+          $psObj->{title} = $pretitle;
+          $psObj->{hrid} = $hrid;
+          $psObj->{precedingInstanceId} = $inst_id;
+          $psObj->{identifiers} = [];
+          foreach my $sn ($field->subfield('x')) {
+            my $identObj = { value => $sn, identifierTypeId => '913300b2-03ed-469a-8179-c1092c991227' };
+            push @{ $psObj->{identifiers} }, $identObj;
+          }
+          foreach my $bn ($field->subfield('z')) {
+            my $identObj = { value => $bn, identifierTypeId => '8261054f-be78-422d-bd51-4ed9f33c3422' };
+            push @{ $psObj->{identifiers} }, $identObj;
+          }
+          push @{ $out->{precedingSucceedingTitles} }, $psObj;
+          # print "$hrid $pretitle\n";
+          $done = 1;
         }
+        last if $done;
       }
+      next if $done;
     }
   }
-  
-  # print OUT $marc->as_usmarc();
+  last if $found >= 10;
 }
+
+print to_json($out, {utf8 => 1, pretty => 1});
