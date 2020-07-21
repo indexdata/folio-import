2715c5bc299719a85b02728cbe5707256e852d31
Charles Ledvina <charles@indexdata.com>
2020-07-21 15:44:33 -0500

    Update sql script.

--- util/winston.sql
+++ util/winston.sql
@@ -1,3 +1,9 @@
+/* Use this script to create a database and table to work with winston-cl-pg.js library */
+
+DROP DATABASE IF EXISTS winston;
+CREATE DATABASE winston;
+\c winston;
+DROP TABLE IF EXISTS winston_logs;
 CREATE TABLE winston_logs
 (
   timestamp timestamp without time zone DEFAULT now(),
