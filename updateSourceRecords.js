10e7ab39b15bc49d4da13091458397847e53a129
Charles Ledvina <charles@indexdata.com>
2020-09-03 09:14:06 -0500

    Read from jsonl

--- updateSourceRecords.js
+++ updateSourceRecords.js
@@ -1,28 +1,38 @@
 /*
-  This script will take a collection or source records and make PUT requests base on the matchedId
+  This script will take a collection in JSONL format of source records and make PUT requests based on the matchedId
 */
 
 const fs = require('fs');
 const superagent = require('superagent');
 const { getAuthToken } = require('./lib/login');
+const readline = require('readline');
 let inFile = process.argv[2];
 
 (async () => {
   try {
     let inData;
     if (!inFile) {
-      throw new Error('Usage: node changeSourceRecods.js <source_record_collection>');
+      throw new Error('Usage: node updateSourceRecords.js <source_record_collection.jsonl>');
     } else if (!fs.existsSync(inFile)) {
       throw new Error('Can\'t find input file');
-    } else {
-      inData = require(inFile);
     }
+
     const config = (fs.existsSync('./config.js')) ? require('./config.js') : require('./config.default.js');
 
     const authToken = await getAuthToken(superagent, config.okapi, config.tenant, config.authpath, config.username, config.password);
+
+    const fileStream = fs.createReadStream(inFile);
+
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+
+    let x = 0;
     
-    for (let x = 0; x < inData.records.length; x++) {
-      let rec = inData.records[x];
+    for await (const line of rl) {
+      x++;
+      let rec = JSON.parse(line);
       let url = `${config.okapi}/source-storage/records/${rec.matchedId}`;
       console.log(`# ${x} PUT to ${url}`);
       try {
@@ -34,7 +44,7 @@ let inFile = process.argv[2];
           .set('accept', 'application/json');
       } catch (e) {
         console.log(e.response || e);
-      } 
+      }
     } 
   } catch (e) {
     console.log(e.message);
