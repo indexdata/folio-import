e0acdf2e0250334b13ff74f9c297f1fcdc223e1f
Charles Ledvina <charles@indexdata.com>
2020-10-28 09:58:00 -0500

    Save into batches of 5000;

--- patrons/splUsers.js
+++ patrons/splUsers.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const uuid = require('uuid/v3');
-const jsonFile = process.argv[2];
 const path = require('path');
+
+const jsonFile = process.argv[2];
+const size = (process.argv[3]) ? parseInt(process.argv[3], 10) : 5000;
 
 const ns = 'dfc59d30-cdad-3d03-9dee-d99117852eab';
 
@@ -25,23 +27,23 @@ const daysNow = Math.floor(new Date().valueOf()/1000/86400);
 
 try {
   if (jsonFile === undefined) {
-    throw new Error('Usage: $ node splUsers.js <borrowers file>');
+    throw new Error('Usage: $ node splUsers.js <borrowers file> [<batch size>]');
   }
   if (!fs.existsSync(jsonFile)) {
     throw new Error('Can\'t find borrowers file');
   }
-
-  // Home address type
-  const addType = '93d3d88d-499b-45d0-9bc7-ac73c3a19880';
 
   const usersIn = require(jsonFile);
   
   const records = {};
   records.users = [];
   let total = 0;
+  let batch = 0;
+  let ttl = 0;
 
   usersIn.forEach(r => {
     total++;
+    ttl++;
     let uukey = r["borrower#"].toString();
     let id = uuid(uukey, 'dfc59d30-cdad-3d03-9dee-d99117852eab');
     let expiry = getDateByDays(r.expiration_date);
@@ -82,17 +84,27 @@ try {
 
     user.username = user.barcode ||user.personal.email || user.externalSystemId || user.personal.lastName + user.personal.firstName;
     records.users.push(user);
+
+    if (total === size || ttl === usersIn.length) {
+      batch++;
+      let set = batch.toString();
+      set = set.padStart(5, '0');
+      let workDir = path.dirname(jsonFile);
+      let fn = path.basename(jsonFile, '.json');
+      let savePath = `${workDir}/users_${set}.json`;
+      records.totalRecords = total;
+      records.deactivateMissingUsers = false;
+      records.updateOnlyPresentFields = false;
+      const recString = JSON.stringify(records, null, 2);
+      console.log(`Writing ${total} records to ${savePath}...`);
+      fs.writeFileSync(savePath, recString);
+      total = 0;
+      records.users = [];
+    }
+
   });
+
   
-  let workDir = path.dirname(jsonFile);
-  let fn = path.basename(jsonFile, '.json');
-  let savePath = `${workDir}/user_import.json`;
-  records.totalRecords = total;
-  records.deactivateMissingUsers = false;
-  records.updateOnlyPresentFields = false;
-  const recString = JSON.stringify(records, null, 2);
-  console.log(`Writing ${total} records to ${savePath}...`);
-  fs.writeFileSync(savePath, recString);
 
 } catch (e) {
   console.error(e.message);
