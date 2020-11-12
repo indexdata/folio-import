c36d9534319851bae2a3865de8bec8922fd0fc89
Charles Ledvina <charles@indexdata.com>
2020-11-12 15:22:46 -0600

    Get async stuff working properly.

--- loadInventorySyncJSONL.js
+++ loadInventorySyncJSONL.js
@@ -2,7 +2,6 @@
 This script will stream instance, holdings, or item json objects from large collections.
 options: 
 -s start record
--r root node (an array of folio records)
 -b batch size
 -u upsert (true or false)
 */
@@ -10,8 +9,7 @@ options:
 const fs = require('fs');
 const superagent = require('superagent');
 const winston = require('winston');
-const JSONStream = require('JSONStream');
-const es = require('event-stream');
+const readline = require('readline');
 const argv = require('minimist')(process.argv.slice(2));
 
 const { getAuthToken } = require('./lib/login');
@@ -42,7 +40,7 @@ const wait = (ms) => {
     const start = new Date().valueOf();
     let inData;
     if (!inFile) {
-      throw new Error('Usage: node loadInventorySyncJSONL.js [options -s start, -r root, -b batch size (default 1000) -u upsert (true|false)] <file>');
+      throw new Error('Usage: node loadInventorySyncJSONL.js [options -s start, -b batch size (default 1000) -u upsert (true|false)] <file>');
     } else if (!fs.existsSync(inFile)) {
       throw new Error('Can\'t find input file');
     }
@@ -73,88 +71,97 @@ const wait = (ms) => {
     let fail = 0;
     let failedRecs = [];
 
-    const runRequest = async (data, es, count, end) => {
-      let date = new Date().toISOString();
-      let endpoint = null;
-      let root = null;
-      if (data.holdingsRecords) {
-        endpoint = '/holdings-storage/batch/synchronous' + upsert;
-        root = 'holdingsRecords'
-      } else if (data.items) {
-        endpoint = '/item-storage/batch/synchronous' + upsert;
-        root = 'items';
-      } else {
-        endpoint = '/instance-storage/batch/synchronous' + upsert;
-        root = 'instances';
-      }
-      const actionUrl = config.okapi + endpoint;
-      const range = `${count}-${end}`;
-      const slice = `${data[root][0].id} - ${data[root][data[root].length - 1].id}`;
-      console.log(`# ${range} Loading section ${slice}`);
-      const ldata = { [root]: data[root] };
-      try {
-        await superagent
-          .post(actionUrl)
-          .send(data)
-          .set('x-okapi-token', authToken)
-          .set('content-type', 'application/json')
-          .set('accept', 'text/plain')
-          .set('connection', 'keep-alive');
-        logger.info(`${date} [${range}] Successfully added record ${slice}`);
-        success++;
-      } catch (e) {
-        logger.error(`${date} [${range}] (${slice}): ${e.response.text}`);
-        if (lpath) {
-          let rfname = lname.replace(/\.json$/, '');
-          fs.writeFileSync(`${lpath}/${rfname}_${range}_err.json`, JSON.stringify(ldata, null, 2));
+    const runRequest = (data, count, end) => {
+      return new Promise(async (resolve, reject) => {
+        let date = new Date().toISOString();
+        let endpoint = null;
+        let root = null;
+        if (data.holdingsRecords) {
+          endpoint = '/holdings-storage/batch/synchronous' + upsert;
+          root = 'holdingsRecords'
+        } else if (data.items) {
+          endpoint = '/item-storage/batch/synchronous' + upsert;
+          root = 'items';
+        } else {
+          endpoint = '/instance-storage/batch/synchronous' + upsert;
+          root = 'instances';
         }
-        fail++;
-      }
-      await wait(config.delay);
-      es.resume(); 
+        const actionUrl = config.okapi + endpoint;
+        const range = `${count}-${end}`;
+        const slice = `${data[root][0].id} - ${data[root][data[root].length - 1].id}`;
+        console.log(`# ${range} Loading section ${slice}`);
+        const ldata = { [root]: data[root] };
+        try {
+          await superagent
+            .post(actionUrl)
+            .send(data)
+            .set('x-okapi-token', authToken)
+            .set('content-type', 'application/json')
+            .set('accept', 'text/plain')
+            .set('connection', 'keep-alive');
+          logger.info(`${date} [${range}] Successfully added record ${slice}`);
+          resolve();
+          success++;
+        } catch (e) {
+          logger.error(`${date} [${range}] (${slice}): ${e.response.text}`);
+          if (lpath) {
+            let rfname = lname.replace(/\.json$/, '');
+            fs.writeFileSync(`${lpath}/${rfname}_${range}_err.json`, JSON.stringify(ldata, null, 2));
+          }
+          fail++;
+          reject();
+        }
+        await wait(config.delay);
+      });
     }
 
-    const stream = fs.createReadStream(inFile, { encoding: "utf8" });
-    let streamCount = 0;
-    let coll = { start: true };
-    let collRoot = null;
-    let startRecord = startRec;
-    stream
-      .pipe(JSONStream.parse(root))
-      .pipe(es.through(function write(data) {
-        if (coll.start) {
-          if (data.instanceId) {
-            collRoot = 'holdingsRecords';
-          } else if (data.holdingsRecordId) {
-            collRoot = 'items';
-          } else {
-            collRoot = 'instances';
-          }
-          coll[collRoot] = [];
-          delete coll.start;
+    const fileStream = fs.createReadStream(inFile, { encoding: "utf8" });
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+
+    let ttl = 0
+    let end = 0;
+    let startNum = 1;
+    let coll = {};
+    
+    await rl.on('close', async () => {
+      
+    });
+
+    for await (const line of rl) {
+      ttl++;
+      let json = JSON.parse(line);
+      if (json.holdingsRecordId) {
+        if (!coll.items) coll.items = [];
+        coll.items.push(json);
+      } else if (json.instanceId) {
+        if (!coll.holdingsRecords) coll.holdingsRecords = [];
+        coll.holdingsRecords.push(json);
+      } else {
+        if (!coll.instances) coll.instances = [];
+        coll.instances.push(json);
+      }
+      if (ttl%collSize === 0) {
+        try {
+          await runRequest(coll, startNum, ttl);
+        } catch (e) {
+          console.log(e);
         }
-        if (streamCount >= startRec) {
-          if (coll[collRoot].length <= collSize) {
-            coll[collRoot].push(data);
-          }
-          if (coll[collRoot].length === collSize) {
-            runRequest(coll, this, startRecord, streamCount);
-            this.pause();
-            coll[collRoot] = [];
-            startRecord = streamCount + 1;
-          }
-        }
-        streamCount++;
-        }, 
-        async function end() {
-          if (coll[collRoot].length > 0) {
-            await runRequest(coll, this, startRecord, streamCount);
-            this.pause();
-          }
-          showStats();
-          this.emit('end')
-        })
-      ); 
+        startNum = ttl + 1;
+        coll = {};
+      }
+    }
+
+    if (Object.keys(coll).length > 0) {
+      try {
+        await runRequest(coll, startNum, ttl);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    
 
     const showStats = () => {
       const end = new Date().valueOf();
