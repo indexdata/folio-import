cf1aada45a32eb22d3d19e9bc19d86ed77ac25de
Charles Ledvina <charles@indexdata.com>
2020-12-15 16:17:55 -0600

    New mod-inventory-update endpoints.

---
+++ bash/put_shared-inventory-upsert-matchkey.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILE=$1;
+if [ -z $FILE ]
+  then
+    echo "Usage: ${0} <inventory_set>"
+    exit
+fi
+
+TMP='./.okapi'
+OKAPI=`cat ${TMP}/url`
+TOKEN=`cat ${TMP}/token`
+
+for f in ${BASH_ARGV[*]}; do
+  echo "Loading ${f}"
+  curl -v --http1.1 -X PUT "${OKAPI}/shared-inventory-upsert-matchkey" -H 'content-type: application/json' -H "x-okapi-token: ${TOKEN}" -d @$f
+done
