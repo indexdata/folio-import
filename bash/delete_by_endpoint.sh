3654240d05076c92d98da57459c096c58aad8401
Charles Ledvina <charles@indexdata.com>
2021-04-06 09:49:03 -0500

    New bash scripts for deleting.

--- bash/delete_by_endpoint.sh
+++ bash/delete_by_endpoint.sh
@@ -4,13 +4,25 @@ TMP='./.okapi'
 OKAPI=`cat ${TMP}/url`
 TOKEN=`cat ${TMP}/token`
 
-EP=$1
-UUID=$2
-
-if [ -z $UUID ] 
+EP=$1;
+UFILE=$2;
+if [ -z $UFILE ]
   then
-    echo "Usage: ${0} <okap_endpoint> <uuid>"
+    echo 'Usage: ./update_by_endpoint <endpoint> <jsonl_file>'
+    exit
+fi
+if [ ! -f $UFILE ]
+  then
+    echo 'File not found'
     exit
 fi
 
-curl --http1.1 -w '\n' -X DELETE "${OKAPI}/${EP}/${UUID}" -H "x-okapi-token: ${TOKEN}"
+LN=1;
+while IFS= read -r line
+  do
+    UUID=`echo $line | grep -E -o '"id":"[^"]+' | grep -E -o -m 1 '........-....-....-....-............'`
+    echo "Deleting # ${LN} -- ${UUID}"
+    curl -w '\n' -X DELETE --http1.1 "${OKAPI}/${EP}/${UUID}" -H "x-okapi-token: ${TOKEN}" 
+    LN=$(expr $LN + 1)
+done < $UFILE
+
