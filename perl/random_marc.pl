861e690cf8b5cb3a0f82b6c16aac366a37eab6f8
Charles Ledvina <charles@indexdata.com>
2021-10-28 09:17:34 -0500

    Short perl script for getting every nth record in a mrc collection.

---
+++ perl/random_marc.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+# This script will take every nth record from a marc collection and save them to a smaller collection.
+
+my $stop = shift;
+my $nth = shift;
+my $marc_file = shift or die "Usage ./random_marc.pl <stop_count> <nth_record> <file>\n";
+my $out_file = $marc_file;
+$out_file =~ s/(.+)\/.*/$1/;
+$out_file .= "/random.mrc";
+unlink($out_file);
+open OUT, ">>", $out_file;
+
+$/ = "\x1D";
+open IN, $marc_file or die "Can't open $marc_file!";
+my $c = 0;
+my $ttl = 0;
+while (<IN>) {
+  if ($c % $nth == 0) {
+    print OUT $_;
+    $ttl++;
+  }
+  $c++;
+  last if $ttl == $stop;
+}
+print "$ttl records saved to $out_file\n";
