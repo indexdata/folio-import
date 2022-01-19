7318160263a722839d2f3d62990317cf291700f6
Charles Ledvina <charles@indexdata.com>
2022-01-19 11:43:00 -0600

    Make fiscal year objects.

--- acquisitions/makeFiscalYear.js
+++ acquisitions/makeFiscalYear.js
@@ -18,11 +18,12 @@ const ns = 'e35dff4e-9035-4d6a-b621-3d42578f81c7';
       fs.writeFileSync(fn, jsonStr, { flag: 'a' });
     }
     const dir = path.dirname(inFile);
+
     const unitsFile = dir + '/acq-units.jsonl';
     if (fs.existsSync(unitsFile)) fs.unlinkSync(unitsFile);
 
     const fyFile = dir + '/fiscal-years.jsonl';
-    if (fs.existsSync(fyFile)) fs.unlinkSync(unitsFile);
+    if (fs.existsSync(fyFile)) fs.unlinkSync(fyFile);
 
     const fileStream = fs.createReadStream(inFile);
 
@@ -31,6 +32,9 @@ const ns = 'e35dff4e-9035-4d6a-b621-3d42578f81c7';
       crlfDelay: Infinity
     });
 
+    let auCount = 0;
+    let fyCount = 0;
+
     const acqUnits = {};
     let x = 0;
     for await (const line of rl) {
@@ -38,8 +42,26 @@ const ns = 'e35dff4e-9035-4d6a-b621-3d42578f81c7';
       if (x > 1) {
         let c = line.split(/\t/);
         let uname = c[1];
-        if (!acqUnits[uname]) acqUnits[uname] = uuid(uname, ns);
-        console.log(line);
+        if (uname && !acqUnits[uname]) acqUnits[uname] = uuid(uname, ns);
+        let code = c[3];
+        let id = uuid(code, ns);
+        let start = new Date(c[6]).toISOString();
+        let end = new Date(c[7]).toISOString();
+        // start = start.replace(/T.+/, 'T00:00:00 -600');
+        // end = end.replace(/T.+/, 'T00:00:00 -600');
+        let obj = {
+          id: id,
+          name: c[2],
+          code: 'TEST' + c[3],
+          description: c[5],
+          periodStart: start,
+          periodEnd: end
+        }
+        if (acqUnits[uname]) {
+          obj.acqUnitIds = [ acqUnits[uname] ];
+        }
+        writeObj(fyFile, obj);
+        fyCount++;
       }
     }
     for (let u in acqUnits) {
@@ -48,7 +70,11 @@ const ns = 'e35dff4e-9035-4d6a-b621-3d42578f81c7';
       obj.id = acqUnits[u];
       obj.isDeleted = false;
       writeObj(unitsFile, obj);
+      auCount++;
     }
+    console.log('---------------------');
+    console.log('Acq Units Created:', auCount);
+    console.log('Fiscal Yr Created:', fyCount);
   } catch (e) {
     console.log(e);
   }
