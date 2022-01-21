5affceebc0a391e48a421826e590194ce4dedd4a
Charles Ledvina <charles@indexdata.com>
2022-01-21 15:46:21 -0600

    Added ledger object creation.

--- acquisitions/makeFiscalYear.js
+++ acquisitions/makeFiscalYear.js
@@ -14,20 +14,23 @@ const fs = require('fs');
 const readline = require('readline');
 const path = require('path');
 const uuid = require('uuid/v5');
+const argv = require('minimist')(process.argv.slice(2));
+
+console.log(argv);
 
 const ns = 'e35dff4e-9035-4d6a-b621-3d42578f81c7';
 
 (async () => {
   try {
-    const inFile = process.argv[2];
+    const inFile = argv._[0];
     if (!inFile) {
-      throw 'Usage: node makeFiscalYear.js <csv_file>';
+      throw 'Usage: node makeFiscalYear.js <tsv_file> [ -l <ledger_tsv_file> ]';
     } else if (!fs.existsSync(inFile)) {
       throw new Error('Can\'t find input file');
     }
     const writeObj = (fn, data) => {
       const jsonStr = JSON.stringify(data);
-      fs.writeFileSync(fn, jsonStr, { flag: 'a' });
+      fs.writeFileSync(fn, jsonStr + '\n', { flag: 'a' });
     }
     const dir = path.dirname(inFile);
 
@@ -36,6 +39,9 @@ const ns = 'e35dff4e-9035-4d6a-b621-3d42578f81c7';
 
     const fyFile = dir + '/fiscal-years.jsonl';
     if (fs.existsSync(fyFile)) fs.unlinkSync(fyFile);
+
+    const ldFile = dir + '/ledgers.jsonl';
+    if (fs.existsSync(ldFile)) fs.unlinkSync(ldFile);
 
     const fileStream = fs.createReadStream(inFile);
 
@@ -46,23 +52,25 @@ const ns = 'e35dff4e-9035-4d6a-b621-3d42578f81c7';
 
     let auCount = 0;
     let fyCount = 0;
+    let ldCount = 0;
 
+    let fyid = '';
     const acqUnits = {};
     let x = 0;
+    let sl = 1;
+    if (argv.h) sl = 1;
     for await (const line of rl) {
       x++;
-      if (x > 1) {
+      if (x > sl) {
         let c = line.split(/\t/);
         let uname = c[1];
         if (uname && !acqUnits[uname]) acqUnits[uname] = uuid(uname, ns);
         let code = c[3];
-        let id = uuid(code, ns);
+        fyid = uuid(code, ns);
         let start = new Date(c[6]).toISOString();
         let end = new Date(c[7]).toISOString();
-        // start = start.replace(/T.+/, 'T00:00:00 -600');
-        // end = end.replace(/T.+/, 'T00:00:00 -600');
         let obj = {
-          id: id,
+          id: fyid,
           name: c[2],
           code: c[3],
           description: c[5],
@@ -84,9 +92,38 @@ const ns = 'e35dff4e-9035-4d6a-b621-3d42578f81c7';
       writeObj(unitsFile, obj);
       auCount++;
     }
+    if (argv.l) {
+      const fileStream = fs.createReadStream(argv.l);
+
+      const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+      });
+      let x = 0;
+      for await (const line of rl) {
+        x++;
+        if (x > sl) {
+          let c = line.split(/\t/);
+          let obj = { 
+            fiscalYearOneId: fyid,
+            name: c[1],
+            code: c[2],
+            description: c[3],
+            ledgerStatus: c[5] || 'Active',
+            acqUnitIds: [ acqUnits[c[11]] ],
+            restrictEncumbrance: false,
+            restrictExpenditures: false
+          };
+          console.log(obj);
+          writeObj(ldFile, obj);
+          ldCount++;
+        }
+      }
+    }
     console.log('---------------------');
     console.log('Acq Units Created:', auCount);
     console.log('Fiscal Yr Created:', fyCount);
+    console.log('Ledgers Created  :', ldCount);
   } catch (e) {
     console.log(e);
   }
