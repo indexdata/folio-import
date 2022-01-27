86eeb39f39aa2bd7d722f275f7df00a48b21fe44
Charles Ledvina <charles@indexdata.com>
2022-01-27 14:21:10 -0600

    A couple more scripts for getting reference data.

---
+++ bash/reference_finance.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TMP='./.okapi'
+OKAPI=`cat ${TMP}/url`
+TOKEN=`cat ${TMP}/token`
+REFDIR=$1
+if [ ! $1 ]
+  then
+    echo "Usage: ${0} <reference_data_directory>"
+    exit
+fi
+
+if [ ! -d $REFDIR ]
+  then
+    mkdir $REFDIR
+fi
+
+EPS='expense-classes fund-types fiscal-years ledgers funds budgets budget-expense-classes groups group-fund-fiscal-years 
+'
+
+for EP in $EPS
+do
+    echo "Fetching ${EP}"
+    curl --http1.1 -o "${REFDIR}/${EP}.json" -w "\n" "${OKAPI}/finance-storage/${EP}?limit=1000" -H "x-okapi-token: ${TOKEN}"
+done
