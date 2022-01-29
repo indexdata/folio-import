f1b978d734cfc4204c37000c3a359f2e7d825db5
Charles Ledvina <charles@indexdata.com>
2022-01-29 10:38:01 -0600

    Add some stats.

--- util/makemap.pl
+++ util/makemap.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
-# This script will take a line delimited instance record collection (as returned
-# by Theodore's Python script) and save a mapping of hrids to instance IDs.
+# This script takes an instance, holdings or item record collection
+# in JSONL format and return an hrid to id map.
 
 use JSON;
 use Data::Dumper;
@@ -9,19 +9,24 @@ use Data::Dumper;
 my $infile = shift;
 
 unless ($infile) {
-  print "Usage $0 <line_delimited_instance_file>\n";
+  print "Usage $0 <jsonl_inventory_file>\n";
   exit;
 }
 
 open IN, $infile or die "Can't open input file!\n";
 
 my $out = {};
+my $count = 0;
 while (<IN>) {
+  $count++;
+  print ("Processed $count records...\n") if $count % 10000 == 0;
   chomp;
   my $jrec = decode_json($_);
   $out->{$jrec->{hrid}} = $jrec->{id};
 }
+print "---------------\nTotal records: $count\n";
+print "Writing to ${infile}-map.json\n";
 my $jout = JSON->new->pretty->encode($out);
 $infile =~ s/\.\w+$//;
-open OUT, ">${infile}.map";
+open OUT, ">${infile}-map.json";
 print OUT $jout;
