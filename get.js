79fbde1382c93430b469984f8fe59a5d085a37b6
Ubuntu <ubuntu@ip-172-31-1-23.us-west-2.compute.internal>
2022-02-04 22:36:14 +0000

    Just a generic get script

---
+++ get.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const superagent = require('superagent');
+const { getAuthToken } = require('./lib/login');
+let ep = process.argv[2];
+
+(async () => {
+  const config = (fs.existsSync('./config.js')) ? require('./config.js') : require('./config.default.js');
+  const authToken = await getAuthToken(superagent, config.okapi, config.tenant, config.authpath, config.username, config.password);
+  let url = `${config.okapi}/${ep}`;
+  console.log('GET', url);
+  try {
+    const res = await superagent
+	.get(url)
+	.set('x-okapi-token', authToken);
+    console.log(JSON.stringify(res.body, null, 2));
+  } catch (e) {
+    console.log(e);
+  }
+})();
