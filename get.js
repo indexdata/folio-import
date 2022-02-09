cd13f26fab27cd355e2159aabc8c6ff1d7f9016a
Charles Ledvina <charles@indexdata.com>
2022-02-09 14:11:42 -0600

    Add autocomplete to get.sh script.

--- get.js
+++ get.js
@@ -4,16 +4,26 @@ const { getAuthToken } = require('./lib/login');
 let ep = process.argv[2];
 
 (async () => {
-  const config = (fs.existsSync('./config.js')) ? require('./config.js') : require('./config.default.js');
-  const authToken = await getAuthToken(superagent, config.okapi, config.tenant, config.authpath, config.username, config.password);
-  let url = `${config.okapi}/${ep}`;
-  console.warn('GET', url);
   try {
-    const res = await superagent
-	.get(url)
-	.set('x-okapi-token', authToken);
-    console.log(JSON.stringify(res.body, null, 2));
-  } catch (e) {
-    console.log(`${e}`);
+    if (!ep) throw(`Usage node get.js <end_point>`);
+    const config = (fs.existsSync('./config.js')) ? require('./config.js') : require('./config.default.js');
+    const authToken = await getAuthToken(superagent, config.okapi, config.tenant, config.authpath, config.username, config.password);
+
+    if (ep.match(/^\.x/)) {
+      ep = ep.replace(/^\.x\//, '');
+      ep = ep.replace(/__/g, '/');
+    }
+    let url = `${config.okapi}/${ep}`;
+    console.warn('GET', url);
+    try {
+      const res = await superagent
+    .get(url)
+    .set('x-okapi-token', authToken);
+      console.log(JSON.stringify(res.body, null, 2));
+    } catch (e) {
+      console.log(`${e}`);
+    }
+  } catch(e) {
+      console.log(`${e}`);
   }
 })();
