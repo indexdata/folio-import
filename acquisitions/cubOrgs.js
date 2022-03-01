d7f0903d74037c6f25280dca2c35fd5dffe7d32a
Charles Ledvina <charles@indexdata.com>
2022-03-01 16:37:17 -0600

    New marc mapping script for Lane.

--- acquisitions/cubOrgs.js
+++ acquisitions/cubOrgs.js
@@ -85,6 +85,7 @@ const col = { a:0, b:1, c:2, d:3, e:4, f:5, g:6, h:7, i:8, j:9, k:10, l:11, m:12
           }
           let url = c[col.n];
           if (url) {
+            if (!url.match(/^http|^ftp/)) url = 'http://' + url;
             let e = { value: url };
             if (!org.urls) {
               e.isPrimary = true;
