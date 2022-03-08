d3761d09ea3849f1a03b330b39018bc60f9e995f
Charles Ledvina <charles@indexdata.com>
2022-03-08 11:39:10 -0600

    Do not circulate items that have location code beginning with 'hdn';

--- perl/marc2inst-hc.pl
+++ perl/marc2inst-hc.pl
@@ -833,8 +833,12 @@ sub make_hi {
       }
       $irec->{copyNumber} = $item->subfield('g') || '';
       $irec->{permanentLoanTypeId} = $sierra2folio->{loantypes}->{$itype} || $refdata->{loantypes}->{'Can Circulate'};
+      if ($loc =~ /^hdn/ && $itype eq '0') {
+        $irec->{materialTypeId} = $refdata->{loantypes}->{'Non Circulating'};
+      }
       $irec->{materialTypeId} = $sierra2folio->{mtypes}->{$itype} || $refdata->{mtypes}->{Other} || die "No material type found for itype $itype\n";
       $irec->{status}->{name} = $sierra2folio->{statuses}->{$status} || 'Unknown'; # defaulting to available;
+ 
       foreach (@msgs) {
         if (!$irec->{circulationNotes}) { $irec->{circulationNotes} = [] }
         my $cnobj = {};
