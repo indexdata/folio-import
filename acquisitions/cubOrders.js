c080ba0e9ee10f35660933bc48901971ca727ea6
Charles Ledvina <charles@indexdata.com>
2022-04-26 17:32:02 -0500

    More mapping.

--- acquisitions/cubOrders.js
+++ acquisitions/cubOrders.js
@@ -4,22 +4,22 @@ const uuid = require('uuid/v5');
 const path = require('path');
 
 const ns = 'e35dff4e-9035-4d6a-b621-3d42578f81c7';
+const unit = '24c4baf7-0653-517f-b901-bde483894fdd';  // CU Boulder
 
 let refDir = process.argv[2];
 const inFile = process.argv[3];
 
 const refFiles = {
   organizations: 'organizations.json',
-  funds: 'funds.json'
+  funds: 'funds.json',
+  entries: 'fund-codes.json'
 };
 
 (async () => {
   try {
-    if (!inFile) throw('Usage: node hcOrders.js <acq_ref_dir> <sierra_orders_json_file>');
+    if (!inFile) throw('Usage: node cubOrders.js <acq_ref_dir> <sierra_orders_json_file>');
     if (!fs.existsSync(inFile)) throw new Error(`Can't find ${inFile}!`);
     refDir = refDir.replace(/\/$/, '');
-    const fundsMapFile = `${refDir}/funds_map.tsv`;
-    if (!fs.existsSync(fundsMapFile)) throw new Error(`Cant't find funds_map.tsv in ${refDir}!`);
 
     const dir = path.dirname(inFile);
     const fn = path.basename(inFile, '.jsonl');
@@ -34,19 +34,15 @@ const refFiles = {
       console.log(`Mapping ${prop}...`);
       obj[prop].forEach(p => {
         let code = p.code;
-        refData[prop][code] = p.id
+        if (prop === 'entries') {
+          let codeNum = `${p.codeNumber}`;
+          codeNum = codeNum.padStart(5, '0');
+          refData[prop][codeNum] = refData.funds[code];
+        } else {
+          refData[prop][code] = p.id;
+        }
       })
     }
-
-    const fundsMap = {};
-    const fundsTsv = fs.readFileSync(fundsMapFile, { encoding: 'utf8'});
-    fundsTsv.split(/\n/).forEach(l => {
-      let [code, num] = l.split(/\t/);
-      if (code) {
-        let numPadded = num.padStart(5, '0');
-        fundsMap[numPadded] = refData.funds[code];
-      }
-    });
 
     const fileStream = fs.createReadStream(inFile);
 
@@ -67,35 +63,53 @@ const refFiles = {
         let poId = uuid(poNum, ns);
         let vcode = ff['22'].value || '';
         vcode = vcode.trim();
-        let orgId = refData.organizations[vcode] || vcode;
+        let orgId = refData.organizations[vcode];
+        if (!orgId) throw(`WARN no vendor code found for "${vcode}" (${poNum})`);
         let co = {
           id: poId,
           poNumber: poNum,
           vendor: orgId,
           dateOrdered: ff['13'].value,
           compositePoLines: [],
-          notes: []
+          notes: [],
+          acqUnitIds: [ unit ]
         }
         let oType = ff['15'].value;
-        co.orderType = (oType.match(/[os]/)) ? 'Ongoing' : 'One-Time';
+        let statCode = ff['20'].value;
+        co.orderType = (statCode.match(/[fz]/) && oType.match(/[dnoqs]/)) ? 'Ongoing' : 'One-Time';
         if (co.orderType === 'Ongoing') {
-          co.ongoing = {};
+          co.ongoing = {
+            interval: 365,
+            isSubscription: true,
+            renewalDate: co.dateOrdered
+          };
         }
-        let pol = {};
-        vf.forEach(v => {
-          if (v.fieldTag === 's') {
-            pol.selector = v.content;
-          } else if (v.fieldTag.match(/[nz]/)) {
-            co.notes.push(v.content); 
-          }
-        });
-        // co.compositePoLines.push(pol);
-        console.log(co);
+        co.workflowStatus = 'Pending';
+
+        // PO lines start here
+
+        let pol = {
+          paymentStatus: 'Awaiting Payment',
+          poLineNumber: poNum + '-1',
+          checkinItems: false
+        };
+        
+        pol.titleOrPackage = so.bibs[0].title;
+        if (so.bibs[0].author) pol.contributors = [ so.bibs[0].author ];
+
+        let form = ff['11'].value;
+        if (form.match(/[s2lmn]/i)) {
+          pol.checkinItems = true;
+        }
+        if (form === 'c' && oType === 'o') pol.checkinItems = true; 
+        
+        co.compositePoLines.push(pol);
+        console.log(JSON.stringify(co, null, 2));
         let coStr = JSON.stringify(co) + '\n';
         fs.writeFileSync(outFile, coStr, { flag: 'a' });
         c++;
       } catch (e) {
-        console.log(`WARN [${lnum}] ${e}`);
+        console.log(`[${lnum}] ${e}`);
       }
     }
     console.log('Orders created', c);    
