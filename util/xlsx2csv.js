46483c6ace040b52ee3522d5046cde14955016be
Charles Ledvina <charles@indexdata.com>
2022-06-01 10:45:51 -0500

    Better error handling.

--- util/xlsx2csv.js
+++ util/xlsx2csv.js
@@ -4,23 +4,25 @@ const fs = require('fs');
 
 const inFile = process.argv[2];
 const type = process.argv[3] || 'csv';
-const dir = path.dirname(inFile);
 const types = {
   csv: ',',
   tsv: '\t'
 }
 
-if (!inFile) throw('Usage: node xlsx2csv.js [ output type: csv | tsv ]')
+try {
+  if (!inFile) throw new Error('Usage: node xlsx2csv.js <csv_file> [ output type: csv | tsv ]')
+  const dir = path.dirname(inFile);
 
-// Read the file using pathname
-const file = xlsx.readFile(inFile);
-for (let sheetName in file.Sheets) {
-  let sheet = file.Sheets[sheetName];
-  let stream = xlsx.stream.to_csv(sheet, { FS: types[type], blankrows: false});
-  let fn = sheetName.replace(/\W+/g, '_');
-  let outFile = dir + '/' + `${fn}.${type}`;
-  console.log(`Writing to ${outFile}`);
-  stream.pipe(fs.createWriteStream(outFile));
+  // Read the file using pathname
+  const file = xlsx.readFile(inFile);
+  for (let sheetName in file.Sheets) {
+    let sheet = file.Sheets[sheetName];
+    let stream = xlsx.stream.to_csv(sheet, { FS: types[type], blankrows: false});
+    let fn = sheetName.replace(/\W+/g, '_');
+    let outFile = dir + '/' + `${fn}.${type}`;
+    console.log(`Writing to ${outFile}`);
+    stream.pipe(fs.createWriteStream(outFile));
+  } 
+} catch (e) {
+  console.error(e.message);
 }
-
-
