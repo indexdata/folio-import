0aa9ec766271df371d2eb90a758118caceec0bd3
Charles Ledvina <charles@indexdata.com>
2022-06-01 15:08:27 -0500

    Delete from perms/users as well

--- bash/delete_single_user.sh
+++ bash/delete_single_user.sh
@@ -12,3 +12,9 @@ if [ -z $ID ]
 fi
 
 curl --http1.1 -w '\n' -X DELETE "${OKAPI}/users/${ID}" -H "x-okapi-token: ${TOKEN}"
+
+PRES=`curl -s --http1.1 -w '\n' "${OKAPI}/perms/users/?query=userId==${ID}" -H "x-okapi-token: ${TOKEN}"`
+PID=`echo $PRES | grep -o '"id":"[^"]*' | grep -o "........-....-....-....-............"`
+
+echo "Deleting $PID from perms/users"
+curl --http1.1 -w '\n' -X DELETE "${OKAPI}/perms/users/${PID}" -H "x-okapi-token: ${TOKEN}"
