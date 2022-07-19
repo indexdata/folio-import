72c44c4f392ca9d3b5717f1737da828ddf0944c1
Charles Ledvina <charles@folio-bastion.folio-dev.indexdata.com>
2022-07-19 15:30:10 +0000

    Update to holycross ongoing users script

--- etc/holycross/holycross_users.sh
+++ etc/holycross/holycross_users.sh
@@ -2,8 +2,12 @@
 
 udir=/data/sftpusers/holycross/users
 wdir=/home/jobber/holycross
+lfile=/home/jobber/holycross/log/users.log
+userfiles=`ls $udir/*.txt 2>/dev/null`
+dt=`date`
+echo "$dt Starting job..." >> $lfile;
 
-if [ -a $udir/*.txt ]
+if [ "$userfiles" ]
 then
 	ts=`date -I`
 	ddir=$udir/Done/$ts
@@ -19,4 +23,6 @@ then
 	mv $udir/*.txt $ddir
 	mv $udir/*.csv $ddir
 	mv $udir/*.json $ddir
+else 
+	echo "  WARN No user file found!" >> $lfile
 fi
