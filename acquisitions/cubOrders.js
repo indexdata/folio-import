a3529db9840ecd790a2d30c404ccdaa31d50fbf6
Charles Ledvina <charles@indexdata.com>
2022-11-09 14:50:05 -0600

    Limit orders creation by created date and type.

--- acquisitions/cubOrders.js
+++ acquisitions/cubOrders.js
@@ -3,6 +3,7 @@ const readline = require('readline');
 const uuid = require('uuid/v5');
 const path = require('path');
 
+let startDate = '2022-07-01';
 const ns = 'e35dff4e-9035-4d6a-b621-3d42578f81c7';
 const nullns = '00000000-0000-0000-0000-000000000000';
 const unit = '24c4baf7-0653-517f-b901-bde483894fdd';  // CU Boulder
@@ -44,6 +45,7 @@ const formMap = {
 };
 
 (async () => {
+  let startTime = new Date().valueOf();
   try {
     if (!inFile) throw('Usage: node cubOrders.js <acq_ref_dir> <sierra_orders_json_file>');
     if (!fs.existsSync(inFile)) throw new Error(`Can't find ${inFile}!`);
@@ -98,175 +100,194 @@ const formMap = {
     let c = 0;
     let lnum = 0;
     let fail = 0;
+    let skipped = 0;
     for await (const line of rl) {
       lnum++;
       try {
         let so = JSON.parse(line);
-        let vf = (so.varFields) ? so.varFields : [];
         let ff = so.fixedFields;
-        let poNum = so.id.toString();
-        let poId = uuid(poNum, ns);
-        let vcode = ff['22'].value || '';
-        vcode = vcode.trim();
-        let orgId = refData.organizations[vcode];
-        if (!orgId) throw(`WARN no organizationId found for "${vcode}" (${poNum})`);
-        let co = {
-          id: poId,
-          poNumber: poNum,
-          vendor: orgId,
-          dateOrdered: (ff['13']) ? ff['13'].value : '',
-          compositePoLines: [],
-          notes: [],
-          acqUnitIds: [ unit ]
-        }
-        let acqType = (ff['1']) ? ff['1'].value : '';
-        let form = (ff['11']) ? ff['11'].value : '';
+        let created = (ff['13']) ? ff['13'].value : '1970-01-01';
         let oType = (ff['15']) ? ff['15'].value : '';
         let statCode = (ff['20']) ? ff['20'].value : '';
-        let raction = (ff['16']) ? ff['16'].value : '';
-        co.orderType = (statCode.match(/[fz]/) && oType.match(/[dnoqs]/)) ? 'Ongoing' : 'One-Time';
-        if (co.orderType === 'Ongoing') {
-          co.ongoing = {
-            interval: 365,
-            isSubscription: true,
-            renewalDate: co.dateOrdered
-          };
-        }
         let status = 'Open';
         if (statCode === '1') {
           status = 'Pending'
         } else if (statCode === 'z') {
           status = 'Closed'
         }
-        co.workflowStatus = status;
-
-        // PO lines start here
-
-        let pol = {
-          paymentStatus: 'Awaiting Payment',
-          poLineNumber: poNum + '-1',
-          source: 'User',
-          checkinItems: false,
-          locations: []
-        };
-
-        pol.rush = (raction.match(/[anrm]/)) ? true : false;
-
-        if (oType === 'a') {
-          pol.acquisitionMethod = refData.acquisitionMethods['Approval Plan'];
-        } else if (oType.match(/[ew]/)) {
-          pol.acquisitionMethod = refData.acquisitionMethods['Demand Driven Acquisitions (DDA)'];
-        } else if (acqType === 'd') {
-          pol.acquisitionMethod = refData.acquisitionMethods['Depository'];
-        } else if (acqType === 'e') {
-          pol.acquisitionMethod = refData.acquisitionMethods['Exchange'];
-        } else if (acqType === 'g') {
-          pol.acquisitionMethod = refData.acquisitionMethods['Gift'];
-        } else if (oType === '2') {
-          pol.acquisitionMethod = refData.acquisitionMethods['Evidence Based Acquisitions (EBA)'];
-        } else {
-          pol.acquisitionMethod = refData.acquisitionMethods['Purchase'];
-        }
-
-        if (!pol.acquisitionMethod) {
-          throw new Error(`No acquisitionMethod found for plNum`);
-        }
-
-        let format;
-        if (form.match(/[erxy4]/)) {
-          format = 'Electronic Resource';
-        } else if (form.match(/[ajuz]/)) {
-          format = 'Other';
-        } else if (form === '2') {
-          format = 'P/E Mix';
-        } else {
-          format = 'Physical Resource';
-        }
-        pol.orderFormat = format;
-
-        let location = ff['2'].value;
-        location = location.trim();
-        let copies = ff['5'].value;
-        copies = parseInt(copies);
-        let price = ff['10'].value;
-        price = price.replace(/(\d\d)\.0+/, '.$1');
-        price = parseFloat(price);
-
-        let loc = {};
-        pol.cost = {};
-        pol.cost.currency = 'USD';
-        if (format === 'Electronic Resource') {
-          pol.cost.listUnitPriceElectronic = price;
-          pol.cost.quantityElectronic = copies;
-          loc.quantityElectronic = copies;
-        } else {
-          pol.cost.listUnitPrice = price;
-          pol.cost.quantityPhysical = copies;
-          loc.quantityPhysical = copies;
-          let mtypeName = '';
-          if (formMap[form]) {
-            mtypeName = formMap[form];
-          } else if (oType === 'z') {
-            mtypeName = 'apc acq';
-          } else if (oType === 'r') {
-            mtypeName = 'annual acq';
-          } else if (oType === 'd' || oType === 'o') {
-            mtypeName = 'so acq';
-          } else if (oType === 'x') {
-            mtypeName = 'manage acq';
+        let orderType = (statCode.match(/[fz]/) && oType.match(/[dnoqs]/)) ? 'Ongoing' : 'One-Time';
+        if (created >= startDate || (status.match(/Open|Pending/) && orderType === 'Ongoing')) {
+          let poNum = so.id.toString();
+          let poId = uuid(poNum, ns);
+          let vcode = ff['22'].value || '';
+          vcode = vcode.trim();
+          let orgId = refData.organizations[vcode];
+          if (!orgId) throw(`ERROR no organizationId found for "${vcode}" (${poNum})`);
+          let co = {
+            id: poId,
+            poNumber: poNum,
+            vendor: orgId,
+            dateOrdered: created,
+            compositePoLines: [],
+            notes: [],
+            acqUnitIds: [ unit ]
           }
-          pol.physical = {
-            createInventory: 'None',
-            materialType: refData.mtypes[mtypeName] || refData.mtypes.unspecified
-          };
-        }
+          let acqType = (ff['1']) ? ff['1'].value : '';
+          let form = (ff['11']) ? ff['11'].value : '';
+          let raction = (ff['16']) ? ff['16'].value : '';
 
-        loc.quantity = copies;
-        loc.locationId = locMap[location];
-        if (!loc.locationId) throw(`WARN no locationId found for "${location}"`)
-        pol.locations.push(loc);
-
-        pol.titleOrPackage = so.bibs[0].title;
-        if (so.bibs[0].author) {
-          let au = {
-            contributor: so.bibs[0].author,
-            contributorNameTypeId: '2b94c631-fca9-4892-a730-03ee529ffe2a' // personal name
+          co.orderType = orderType;
+          if (co.orderType === 'Ongoing') {
+            co.ongoing = {
+              interval: 365,
+              isSubscription: true,
+              renewalDate: co.dateOrdered
+            };
           }
-          pol.contributors = [ au ];
-        }
-        pol.publicationDate = `${so.bibs[0].publishYear}`;
-        let bibId = so.bibs[0].id;
-        bibId = 'b' + bibId;
-        pol.instanceId = uuid(bibId + ver, nullns);
-        pol.description = bibId;
+          
+          co.workflowStatus = status;
 
-        let fundNum = ff['12'].value || '';
-        let fundId = refData.entries[fundNum] || '';
-        if (fundId) {
-          let fd = {
-            fundId: fundId,
-            distributionType: 'percentage',
-            value: 100
+          // PO lines start here
+
+          let pol = {
+            paymentStatus: 'Awaiting Payment',
+            poLineNumber: poNum + '-1',
+            source: 'User',
+            checkinItems: false,
+            locations: []
           };
-          pol.fundDistribution = [ fd ];
-        }
 
-        if (form.match(/[s2lmn]/i)) {
-          pol.checkinItems = true;
+          pol.rush = (raction.match(/[anrm]/)) ? true : false;
+
+          if (oType === 'a') {
+            pol.acquisitionMethod = refData.acquisitionMethods['Approval Plan'];
+          } else if (oType.match(/[ew]/)) {
+            pol.acquisitionMethod = refData.acquisitionMethods['Demand Driven Acquisitions (DDA)'];
+          } else if (acqType === 'd') {
+            pol.acquisitionMethod = refData.acquisitionMethods['Depository'];
+          } else if (acqType === 'e') {
+            pol.acquisitionMethod = refData.acquisitionMethods['Exchange'];
+          } else if (acqType === 'g') {
+            pol.acquisitionMethod = refData.acquisitionMethods['Gift'];
+          } else if (oType === '2') {
+            pol.acquisitionMethod = refData.acquisitionMethods['Evidence Based Acquisitions (EBA)'];
+          } else {
+            pol.acquisitionMethod = refData.acquisitionMethods['Purchase'];
+          }
+
+          if (!pol.acquisitionMethod) {
+            throw new Error(`No acquisitionMethod found for plNum`);
+          }
+
+          let format;
+          if (form.match(/[erxy4]/)) {
+            format = 'Electronic Resource';
+          } else if (form.match(/[ajuz]/)) {
+            format = 'Other';
+          } else if (form === '2') {
+            format = 'P/E Mix';
+          } else {
+            format = 'Physical Resource';
+          }
+          pol.orderFormat = format;
+
+          let location = ff['2'].value;
+          location = location.trim();
+          let copies = ff['5'].value;
+          copies = parseInt(copies);
+          let price = ff['10'].value;
+          price = price.replace(/(\d\d)\.0+/, '.$1');
+          price = parseFloat(price);
+
+          let loc = {};
+          pol.cost = {};
+          pol.cost.currency = 'USD';
+          if (format === 'Electronic Resource') {
+            pol.cost.listUnitPriceElectronic = price;
+            pol.cost.quantityElectronic = copies;
+            loc.quantityElectronic = copies;
+          } else {
+            pol.cost.listUnitPrice = price;
+            pol.cost.quantityPhysical = copies;
+            loc.quantityPhysical = copies;
+            let mtypeName = '';
+            if (formMap[form]) {
+              mtypeName = formMap[form];
+            } else if (oType === 'z') {
+              mtypeName = 'apc acq';
+            } else if (oType === 'r') {
+              mtypeName = 'annual acq';
+            } else if (oType === 'd' || oType === 'o') {
+              mtypeName = 'so acq';
+            } else if (oType === 'x') {
+              mtypeName = 'manage acq';
+            }
+            pol.physical = {
+              createInventory: 'None',
+              materialType: refData.mtypes[mtypeName] || refData.mtypes.unspecified
+            };
+          }
+
+          loc.quantity = copies;
+          loc.locationId = locMap[location];
+          if (!loc.locationId) throw(`ERROR no locationId found for "${location}"`)
+          pol.locations.push(loc);
+
+          pol.titleOrPackage = so.bibs[0].title;
+          if (so.bibs[0].author) {
+            let au = {
+              contributor: so.bibs[0].author,
+              contributorNameTypeId: '2b94c631-fca9-4892-a730-03ee529ffe2a' // personal name
+            }
+            pol.contributors = [ au ];
+          }
+          pol.publicationDate = `${so.bibs[0].publishYear}`;
+          let bibId = so.bibs[0].id;
+          bibId = 'b' + bibId;
+          pol.instanceId = uuid(bibId + ver, nullns);
+          pol.description = bibId;
+
+          let fundNum = ff['12'].value || '';
+          let fundId = refData.entries[fundNum] || '';
+          if (fundId) {
+            let fd = {
+              fundId: fundId,
+              distributionType: 'percentage',
+              value: 100
+            };
+            pol.fundDistribution = [ fd ];
+          }
+
+          if (form.match(/[s2lmn]/i)) {
+            pol.checkinItems = true;
+          }
+          if (form === 'c' && oType === 'o') pol.checkinItems = true; 
+          
+          co.compositePoLines.push(pol);
+          let coStr = JSON.stringify(co) + '\n';
+          fs.writeFileSync(outFile, coStr, { flag: 'a' });
+          c++;
+        } else {
+          let reason;
+          if (status === 'Closed') {
+            reason = `it has a status of ${status}`;
+          } else {
+            reason = `it was created before ${startDate} on ${created}`; 
+          }
+          console.log(`[${lnum}] INFO Skipping ${so.id} since ${reason}`);
+          skipped++;
         }
-        if (form === 'c' && oType === 'o') pol.checkinItems = true; 
-        
-        co.compositePoLines.push(pol);
-        let coStr = JSON.stringify(co) + '\n';
-        fs.writeFileSync(outFile, coStr, { flag: 'a' });
-        c++;
       } catch (e) {
         console.log(`[${lnum}] ${e}`);
         fail++;
       }
     }
+    const endTime = new Date().valueOf();
+    const ttime = (endTime - startTime)/1000;
+    console.log('Total time (seconds)', ttime);
     console.log('Orders created', c);
     console.log('Orders failed', fail);
+    console.log('Orders skipped', skipped);
   } catch (e) {
     console.log(e);
   }
