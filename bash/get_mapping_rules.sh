08c2a19e29e3b07a72fbbe5cd5656de990af5447
Charles Ledvina <charles@indexdata.com>
2022-11-10 14:03:36 -0600

    Get initial control number stuff ironed out.

--- bash/get_mapping_rules.sh
+++ bash/get_mapping_rules.sh
@@ -4,4 +4,11 @@ TMP='./.okapi'
 OKAPI=`cat ${TMP}/url`
 TOKEN=`cat ${TMP}/token`
 
-curl --http1.1 -w '\n' "${OKAPI}/mapping-rules" -H "x-okapi-token: ${TOKEN}"
+TYPE=$1
+if [ ! $TYPE ] 
+then
+	echo "Usage: $0 <type: marc-bib, marc-holdings, marc-authority>"
+	exit
+fi
+
+curl --http1.1 -w '\n' "${OKAPI}/mapping-rules/$TYPE" -H "x-okapi-token: ${TOKEN}"
