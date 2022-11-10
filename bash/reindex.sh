a15513e4a6a0f7f3a94f042937628ff92a90fd10
Charles Ledvina <charles@indexdata.com>
2022-11-10 15:30:13 -0600

    Add payload to reindex script.

--- bash/reindex.sh
+++ bash/reindex.sh
@@ -4,4 +4,4 @@ TMP='./.okapi'
 OKAPI=`cat ${TMP}/url`
 TOKEN=`cat ${TMP}/token`
 
-curl -w '\n' --http1.1 "${OKAPI}/search/index/inventory/reindex" -H 'content-type: application/json' -H "x-okapi-token: ${TOKEN}" -d ''
+curl -w '\n' --http1.1 "${OKAPI}/search/index/inventory/reindex" -H 'content-type: application/json' -H "x-okapi-token: ${TOKEN}" -d '{"recreateIndex":true,"resourceName":"instance"}'
