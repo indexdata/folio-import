aaf5af4d3d9af5e587072ab4b95fa4ebd5b3ccdb
Charles Ledvina <charles@indexdata.com>
2022-12-08 13:33:02 -0600

    Save authToken to .okapi file.

--- authToken.js
+++ authToken.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 const superagent = require('superagent');
 const { getAuthToken } = require('./lib/login');
+const okapi = '.okapi';
 
 (async () => {
   const config = (fs.existsSync('./config.js')) ? require('./config.js') : require('./config.default.js');
   const authToken = await getAuthToken(superagent, config.okapi, config.tenant, config.authpath, config.username, config.password);
-  console.log(authToken);
+  const out = {
+    url: config.okapi,
+    tenant: config.tenant,
+    token: authToken
+  };
+  fs.writeFileSync(okapi, JSON.stringify(out));
+  console.log(out);
 })();
