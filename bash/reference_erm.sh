8dd5a2cde4ee21660de91ed0229941cbb1478b26
Charles Ledvina <charles@indexdata.com>
2023-05-11 13:26:17 -0500

    New script for download erm reference data.

---
+++ bash/reference_erm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TMP='./.okapi'
+OKAPI=`cat ${TMP}/url`
+TOKEN=`cat ${TMP}/token`
+REFDIR=$1
+if [ ! $1 ]
+  then
+    echo "Usage: ${0} <reference_data_directory>"
+    exit
+fi
+
+if [ ! -d $REFDIR ]
+  then
+    mkdir $REFDIR
+fi
+
+EPS='erm/refdata'
+
+for EP in $EPS
+do
+    FN=`echo $EP | sed 's/.*\///'`;
+    echo "Fetching ${EP}"
+    curl --http1.1 -o "${REFDIR}/${FN}.json" -w "\n" "${OKAPI}/${EP}?limit=1000" -H "x-okapi-token: ${TOKEN}"
+done
