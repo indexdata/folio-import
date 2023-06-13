5c9c0a6c09e1d9bedeb5ccb39f831fb597d9da80
Charles Ledvina <charles@indexdata.com>
2023-06-13 16:31:49 -0500

    Start of user import script for CU.

---
+++ patrons/cubUserImport.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+const inFile = process.argv[2];
+
+(async () => {
+  try {
+    if (!inFile) {
+      throw('Usage: node cubUserImport.js <cu-user-import-file>');
+    }
+    if (!fs.existsSync(inFile)) {
+      throw(`Can't find user file at ${inFile}`);
+    }
+    const dir = path.dirname(inFile);
+    let fn = path.basename(inFile);
+    let fullPath = dir + '/user-import-' + fn;
+
+    const users = require(inFile);
+    const ui = { users: [] };
+    for (let x = 0; x < users.length; x++) {
+      let u = users[x];
+      if (u.personal && u.personal.addresses) {
+        for (let y = 0; y < u.personal.addresses.length; y++) {
+          let a = u.personal.addresses[y];
+          if (a.postal) {
+            a.postalCode = a.postal;
+            delete a.postal;
+          }
+        }
+      }
+      ui.users.push(u);
+    }
+    let ttl = ui.users.length;
+    ui.totalRecords = ttl;
+    ui.updateOnlyPresentFields = true;
+    let outStr = JSON.stringify(ui, null, 2);
+    console.log(outStr);
+    fs.writeFileSync(fullPath, JSON.stringify(ui, null, 2));
+    console.log('Total users', ttl);
+  } catch (e) {
+    console.log(e);
+  }
+})();
