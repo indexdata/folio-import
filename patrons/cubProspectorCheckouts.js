d387635a6fb03659a815b580cd051796f4ae5ce9
Charles Ledvina <charles@indexdata.com>
2023-06-15 13:37:23 -0500

    Checkout to PROSPECTORMC.

--- patrons/cubProspectorCheckouts.js
+++ patrons/cubProspectorCheckouts.js
@@ -2,23 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 
-const spFile = process.argv[2];
-const usersFile = process.argv[3];
-const itemFile = process.argv[4];
+const ubcode = 'PROSPECTORMC';
+
+const itemFile = process.argv[2];
 
 (async () => {
   try {
     if (itemFile === undefined) {
-      throw('Usage: node cubCheckouts.js <service_points_file> <sierra_users_file> <sierra_checkouts_file>');
+      throw('Usage: node cubProspectorCheckouts.js <sierra_checkouts_file>');
     }
     if (!fs.existsSync(itemFile)) {
       throw new Error('Can\'t find loans file');
-    }
-    if (!fs.existsSync(spFile)) {
-      throw new Error('Can\'t find service points file');
-    }
-    if (!fs.existsSync(usersFile)) {
-      throw new Error('Can\'t find users file');
     }
 
     let dateOffset = (dt) => {
@@ -33,9 +27,7 @@ const itemFile = process.argv[4];
     };
 
     const files = {
-      co: 'checkouts.jsonl',
-      ia: 'inactive_users.jsonl',
-      nf: 'notfound_checkouts.jsonl'
+      co: 'pcheckouts.jsonl',
     };
 
     let workDir = path.dirname(itemFile);
@@ -47,19 +39,6 @@ const itemFile = process.argv[4];
     }
 
     const start = new Date().valueOf();
-    const sp = require(spFile);
-    spMap = {};
-    sp.servicepoints.forEach(s => {
-      spMap[s.name] = s.id;
-    });
-
-    const loc2sp = {};
-    const lospData = fs.readFileSync(workDir + '/spoints.tsv', { encoding: 'utf8' });
-    lospData.split(/\r?\n/).forEach(l => {
-      let [k, x, v] = l.split(/\t/);
-      loc2sp[k] = spMap[v] || v;
-    });
-    // console.log(loc2sp); return;
 
     const main = () => {
       const fileStream = fs.createReadStream(itemFile);
@@ -79,86 +58,42 @@ const itemFile = process.argv[4];
       rl.on('line', l => {
         c++;
         let co = JSON.parse(l);
-        let pnum = (co.patron) ? co.patron.replace(/^.+\//, '') : '';
-        if (pnum) {
-          let user = active[pnum];
-          if (!user) { 
-            console.log(`User not found for ${pnum}`);
-          } else {
-            let ubcode = user.barcode;
-            let odate = co.outDate;
-            let due = co.dueDate;
-            // let loc = (ff['64']) ? ff['64'].value : '';
-            let loc = 'xxxxx';
-            let rnum = co.numberOfRenewals;
-            let ibcode = co.barcode;
-            let loan = {};
-            loan.itemBarcode = ibcode.trim();
-            loan.userBarcode = ubcode.trim();
-            loan.loanDate = odate;
-            loan.dueDate = due;
-            if (rnum) loan.renewalCount = parseInt(rnum, 10);
-            loan.servicePointId = loc2sp[loc] || spMap['Norlin East Desk'];
-            if (process.env.DEBUG) console.log(loan);
-            if (user) {
-              write(files.co, loan);
-              ttl.co++;
-              if (!user.active) {
-                user.barcode = loan.userBarcode;
-                write(files.ia, user);
-                ttl.ia++;
-              }
-            } else {
-              write(files.nf, loan);
-              ttl.nf++;
-            }
-            
-            // if (ttl.co === 10) rl.close();
-          }
+        let sid = co.id;
+        let odate = co.outDate;
+        let due = co.dueDate;
+        let rnum = co.numberOfRenewals;
+        let ibcode = co.barcode;
+        let loan = {};
+        if (ibcode && co.patron && co.patron.match(/@/)) {
+          loan.itemBarcode = ibcode.trim();
+          loan.userBarcode = ubcode;
+          loan.loanDate = odate;
+          loan.dueDate = due;
+          if (rnum) loan.renewalCount = parseInt(rnum, 10);
+          loan.servicePointId = '3a40852d-49fd-4df2-a1f9-6e2641a6e91f';
+          if (process.env.DEBUG) console.log(loan);
+          write(files.co, loan);
+          ttl.co++;
+        } else {
+          if (!ibcode) console.log(`No barcode found for ${sid}`);
+          ttl.nf++;
         }
         if (c % 100000 === 0) {
-          console.log('Items processed', c);
+          console.log('Checkouts processed', c);
         }
       });
       rl.on('close', () => {
         const end = new Date().valueOf();
         const time = (end - start) / 1000;
         console.log('Checkouts:', ttl.co);
-        console.log('Inactives:', ttl.ia);
-        console.log('Proxy COs:', ttl.pr);
-        console.log('Not found:', ttl.nf);
+        console.log('Skipped:', ttl.nf);
         console.log('Time (sec):', time);
       });
     } 
+
+    main();
     
-    const ufileStream = fs.createReadStream(usersFile);
-    const url = readline.createInterface({
-      input: ufileStream,
-      crlfDelay: Infinity
-    });
-
-    console.log('Loading users into memory...')
-
-    let active = {};
-    let ucount = 0;
-    url.on('line', l => {
-      let u = JSON.parse(l);
-      let exDate = (u.fixedFields['43']) ? u.fixedFields['43'].value : '';
-      let exDateVal = (exDate) ? new Date(exDate).valueOf() : 0;
-      let act = (start > exDateVal) ? false : true;
-      let bc = '';
-      u.varFields.forEach(v => {
-        if (v.fieldTag === 'b' && !bc) bc = v.content;
-      });
-
-      active[u.id] = { active: act, expirationDate: exDate, barcode: bc };
-      ucount++;
-    });
-    url.on('close', l=> {
-      console.log(`(${ucount} users loaded...)`);
-      main();
-    });
-  } catch (e) {
+    } catch (e) {
     console.error(e);
   }
 })();
