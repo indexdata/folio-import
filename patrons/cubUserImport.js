626d3c73649efeedabf38fd08d408a94434ee092
Charles Ledvina <charles@indexdata.com>
2023-06-19 16:05:27 -0500

    Reject null usernames.

--- patrons/cubUserImport.js
+++ patrons/cubUserImport.js
@@ -13,14 +13,12 @@ const inFile = process.argv[2];
     }
     const dir = path.dirname(inFile);
     let fn = path.basename(inFile);
-    let fullPath = dir + '/user-import-' + fn;
+    let fullPath = dir + '/ui-' + fn;
 
     const users = require(inFile);
-    const ui = { users: [] };
-    ui.included = { departments: [] };
-    let deptSeen = {};
-    for (let x = 0; x < users.length; x++) {
-      let u = users[x];
+    const ui = { users: [], included: users.included };
+    for (let x = 0; x < users.users.length; x++) {
+      let u = users.users[x];
       if (u.personal && u.personal.addresses) {
         for (let y = 0; y < u.personal.addresses.length; y++) {
           let a = u.personal.addresses[y];
@@ -30,24 +28,16 @@ const inFile = process.argv[2];
           }
         }
       }
-      if (u.included && u.included.departments) {
-        u.departments = [];
-        u.included.departments.forEach(i => {
-          let d = i.name;
-          u.departments.push(d);
-          if (!deptSeen[d]) ui.included.departments.push(i);
-          deptSeen[d] = 1;
-        });
-        delete u.included;
+      if (u.username) {
+        ui.users.push(u);
       }
-      ui.users.push(u);
     }
     let ttl = ui.users.length;
     ui.totalRecords = ttl;
     ui.updateOnlyPresentFields = true;
     let outStr = JSON.stringify(ui, null, 2);
-    console.log(outStr);
-    fs.writeFileSync(fullPath, JSON.stringify(ui, null, 2));
+    // console.log(outStr);
+    fs.writeFileSync(fullPath, JSON.stringify(ui, null, 2) + '\n');
     console.log('Total users', ttl);
   } catch (e) {
     console.log(e);
