7d422cac4d614982a18ce42ca89ba4f0d12c1f6a
Charles Ledvina <charles@indexdata.com>
2023-07-11 15:47:36 -0500

    Get accounts right.

--- patrons/sulFines.js
+++ patrons/sulFines.js
@@ -3,7 +3,7 @@ const path = require('path');
 const parse = require('csv-parse/lib/sync');
 const uuid = require('uuid/v5');
 const superagent = require('superagent');
-const usersFile = process.argv[2];
+let sharedDir = process.argv[2];
 const csvFile = process.argv[3];
 
 const ns = 'b1c087ae-d03e-4959-ad37-e97fc4cdf816';
@@ -12,12 +12,12 @@ const tenant = 'sul';
 (async () => {
   try {
     if (csvFile === undefined) {
-      throw('Usage: node sulFines.js <users_file> <feefines_csv_file>');
+      throw('Usage: node sulFines.js <shared_dir> <feefines_csv_file>');
     }
     if (!fs.existsSync(csvFile)) {
       throw new Error('Can\'t find feefines file');
     }
-    if (!fs.existsSync(usersFile)) {
+    if (!fs.existsSync(sharedDir)) {
       throw new Error('Can\'t find users file');
     }
 
@@ -28,7 +28,9 @@ const tenant = 'sul';
     let token = config.token;
 
     let workDir = path.dirname(csvFile);
+    sharedDir = sharedDir.replace(/\/$/, '');
 
+    const usersFile = sharedDir + '/' + 'users.json';
     const users = require(usersFile);
     delete require.cache[require.resolve(usersFile)];
 
@@ -41,6 +43,27 @@ const tenant = 'sul';
     });
     console.log(`(${uc} user records read)`);
 
+    const ffFile = sharedDir + '/' + 'feefines.json';
+    const ff = require(ffFile);
+    ffmap = {};
+    ff.feefines.forEach(f => {
+      let k = f.feeFineType.toLowerCase();
+      ffmap[k] = { id: f.id, type: f.feeFineType };
+    });
+
+    const ofile = sharedDir + '/' + 'owners.json';
+    const owners = require(ofile);
+    omap = {};
+    owners.owners.forEach(o => {
+      let k = o.owner.toLowerCase();
+      omap[k] = { id: o.id, owner: o.owner };
+    });
+    // console.log(omap); return;
+
+    const writeObj = (fileName, data) => {
+      fs.writeFileSync(fileName, JSON.stringify(data) + '\n', { flag: 'a' });
+    };
+
     let csv = fs.readFileSync(csvFile, { encoding: 'utf8'});
     csv = csv.replace(/^\uFEFF/, ''); // remove BOM
 
@@ -51,7 +74,8 @@ const tenant = 'sul';
 
     const files = {
       ac: 'accounts.jsonl',
-      ff: 'feefineactions.jsonl'
+      ff: 'feefineactions.jsonl',
+      nf: 'user-not-found.jsonl'
     };
 
     for (let k in files) {
@@ -73,17 +97,29 @@ const tenant = 'sul';
       let am = r.amount;
       let re = r.remaining;
       let ps = r.paymentStatus;
+      let dc = r.dateCreated;
+      dc = dc.replace(/(\d{4})(..)(..)/, '$1-$2-$3');
+      let ft = r.feeFinetype.toLowerCase();
+      let fft = ffmap[ft] || {};
+      let owner = r.feeFineOwner.toLowerCase();
+      let own = omap[owner] || {};
+
       if (uid) {
         // now that we have a user ID, let's see if we can't find the item ID
         let acc = {
           userId: uid,
           amount: am,
           remaining: re,
-          paymentStatus: ps
+          paymentStatus: { name: ps },
+          feeFineId: fft.id,
+          feeFineType: fft.type,
+          ownerId: own.id,
+          feeFineOwner: own.owner, 
+          status: { name: 'Open' }
         }
+        acc.dateCreated = dc;
         try {
-          
-          let url = `${base}/item-storage/items?query=barcode==${ibc}`;
+          let url = `${base}/inventory/items?query=barcode==${ibc}`;
           let res = await superagent
             .get(url)
             .set('x-okapi-token', token)
@@ -92,18 +128,40 @@ const tenant = 'sul';
           let iid = (item) ? item.id : '';
           if (iid) {
             acc.itemId = iid;
+            acc.id = uuid(uid + iid + fft + am, ns);
+            acc.title = item.title;
+            acc.callNumber = item.callNumber;
+            acc.barcode = item.barcode;
+            acc.materialType = item.materialType.name;
+            acc.materialTypeId = item.materialType.id;
+            acc.itemStatus = { name: item.status.name};
+            acc.location = item.permanentLocation.name;
+            acc.holdingsRecordId = item.holdingsRecordId;
+            if (item.contributorNames) acc.contributors = item.contributorNames;
           }
           else {
             acc.title = ti;
-            console.log(`WARN Item records not found for barcode ${ibc}`);
+            acc.id = uuid(uid + ti + fft + am, ns);
+            console.log(`WARN Item record not found for barcode ${ibc}`);
           }
+          writeObj(files.ac, acc);
           console.log(acc);
+
+          // make feefine actions
+          let ffa = {
+            id: uuid(acc.id, ns),
+            accountId: acc.id,
+            userId: uid
+          }
+          writeObj(files.ff, ffa);
+          console.log(ffa);
         } catch (e) {
           console.log(e);
         }
         succ++
       } else {
         console.log(`ERROR User not for with barcode ${ubc}!`);
+        writeObj(files.nf, r);
         err++;
       }
     }
