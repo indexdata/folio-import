a168046fa796d4493800e384db0036fea0f39236
Charles Ledvina <charles@indexdata.com>
2023-08-22 15:50:21 -0500

    Finish google notes fixer.

--- inventory/googl-notes-fix.js
+++ inventory/googl-notes-fix.js
@@ -41,14 +41,12 @@ const nnt = '8d0a5eca-25de-4391-81a9-236eeefdd20b';
       files[f] = fullPath;
     }
 
-    // get instance map
-    console.log('Making instance map...')
-    let instMap = {}
     let fileStream = fs.createReadStream(itemFile);
     let rl = readline.createInterface({
       input: fileStream,
       crlfDelay: Infinity
     });
+    let c = 0;
     for await (let line of rl) {
       let i = JSON.parse(line);
       let hrid = 'i' + i.id;
@@ -57,24 +55,43 @@ const nnt = '8d0a5eca-25de-4391-81a9-236eeefdd20b';
       let notes = [];
       i.varFields.forEach(f => {
         if (!vf[f.fieldTag]) vf[f.fieldTag] = [];
-        vf[f.fieldTag].push(f.content);
+        if (f.content) vf[f.fieldTag].push(f.content);
       });
       let icode = ff['59'].value;
       let obj = {
         hrid: hrid,
       }
-      if (icode === '7781' && vf.x) {
+      let xused = 0;
+      if (vf.x) {
         vf.x.forEach(x => {
-          let n = {
-            note: x,
-            itemNoteTypeId: gnt
-          };
+          if (icode === '7781' && x.match(/Google/)) {
+            let n = noteGen(x, gnt, true);
+            notes.push(n);
+            xused = 1;
+          } else {
+            let n = noteGen(x, nnt, true);
+            notes.push(n);
+          }
+        });
+      }
+      if (vf.w) {
+        vf.w.forEach(x => {
+          let n = noteGen(x, nnt, true);
           notes.push(n);
-        })
+        });
+      }
+      if (icode && icode.match(/7777|7778|7779|7780|7781|7786|7789/) && !xused) {
+        let n = noteGen(icode, gnt, true);
+        notes.push(n);
       }
       obj.notes = notes;
-      console.log(obj);
+      writeJSON(files.items, obj)
+      c++;
+      if (c % 10000 === 0) {
+        console.log('Processed', c);
+      }
     }
+    console.log('Done:', c);
   } catch (e) {
     console.log(e);
   }
