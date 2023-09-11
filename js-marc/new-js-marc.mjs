cb9c532859c26fa4ba6d38c3ccf3b9e11f5bd70e
Charles Ledvina <charles@indexdata.com>
2023-09-11 16:10:45 -0500

    New js marc script for make catDate objects.

---
+++ js-marc/new-js-marc.mjs
@@ -0,0 +1,59 @@
+export function parseMarc(raw) {
+  let record = {};
+  let leader = raw.substring(0, 24);
+
+  let dirEnd = parseInt(leader.substring(12, 17), 10);
+  let dir = raw.substring(24, dirEnd);
+  let dirParts = dir.match(/.{12}/g);
+  let mainRec = raw.substring(dirEnd);
+  // console.log(mainRec);
+  let mij = {
+    leader: leader,
+    fields: []
+  };
+  let fields = {
+    leader: leader,
+  };
+  let df = mainRec.split(/\x1E/);
+  df.pop();
+  dirParts.forEach((d, i) => {
+    let p = d.match(/^(.{3})(.{4})(.{5})/);
+    let tag = p[1];
+    let data = df[i];
+    let obj = {};
+    if (!fields[tag]) fields[tag] = [];
+    if (tag > '009') {
+      obj.ind1 = data.substring(0, 1);
+      obj.ind2 = data.substring(1, 2);
+      let subs = data.split(/\x1F/);
+      subs.shift();
+      obj.subfields = [];
+      subs.forEach(s => {
+        let code = s.substring(0, 1);
+        let data = s.substring(1);
+        let sub = { [code]: data };
+        obj.subfields.push(sub);
+      });
+      mij.fields.push({ [tag]: obj });
+      fields[tag].push(obj);
+    } else {
+      mij.fields.push({ [tag]: data });
+      fields[tag].push(data);
+    }
+
+  });
+  record = { mij: mij, fields: fields };
+  return record;
+}
+
+export function getSubs(field, codes, delim) {
+  let dl = (delim) ? delim : ' ';
+  let out = [];
+  field.subfields.forEach(s => {
+    let code = Object.keys(s)[0];
+    if (codes.match(code)) {
+      out.push(s[code]);
+    }
+  });
+  return out.join(dl);
+}
---
+++ js-marc/scripts/catdate.js
@@ -0,0 +1,74 @@
+import { parseMarc } from '../new-js-marc.mjs';
+import { getSubs } from '../new-js-marc.mjs';
+import fs from 'fs';
+import path from 'path';
+
+let rawFile = process.argv[2];
+const schemaDir = './schemas';
+let ldr = '';
+
+try {
+  if (!rawFile) { throw "Usage: node catdata.js <raw_marc_file>" }
+
+  let start = new Date().valueOf();
+
+  let dir = path.dirname(rawFile);
+  let fn = path.basename(rawFile);
+  let outFile = `${dir}/${fn}-catdates.jsonl`;
+  if (fs.existsSync(outFile)) fs.unlinkSync(outFile);
+
+  const fileStream = fs.createReadStream(rawFile, { encoding: 'utf8' });
+  let count = 0;
+  let t;
+  let leftOvers = '';
+  fileStream.on('data', (chunk) => {
+    let recs = chunk.match(/.+?\x1D|.+$/g);
+    recs[0] = leftOvers + recs[0];
+    let lastRec = recs[recs.length - 1];
+    if (!lastRec.match(/\x1D/)) {
+      leftOvers = lastRec;
+      recs.pop();
+    } else {
+      leftOvers = '';
+    }
+    recs.forEach(r => {
+      count++
+      let out = {};
+      let marc = parseMarc(r);
+      let f907 = marc.fields['907'];
+      let hrid = getSubs(f907[0], 'a')
+      hrid = hrid.replace(/.$/, '');
+      let f998 = marc.fields['998'];
+      let cdate = getSubs(f998[0], 'b');
+      let dmy = cdate.split(/-/);
+      let dd = dmy[0];
+      let mm = dmy[1];
+      let yr = dmy[2];
+      if (yr.match(/^[012]/)) {
+        yr = '20' + yr;
+      } else {
+        yr = '19' + yr;
+      }
+      out.hrid = hrid;
+      out.catalogedDate = `${yr}-${mm}-${dd}`;
+      let outStr = JSON.stringify(out);
+      fs.writeFileSync(outFile, outStr + '\n', {flag: 'a'});
+
+      if (process.env.DEBUG) console.log(outStr);
+
+      if (count % 10000 === 0) {
+        let now = new Date().valueOf();
+        t = (now - start) / 1000;
+        console.log('Records processed', count, `${t} secs.`);
+      } 
+    });
+  });
+  fileStream.on('close', () => {
+    let now = new Date().valueOf();
+    let t = (now - start) / 1000;
+    console.log('--------------------');
+    console.log('Records processed', count, `${t} secs.`);
+  });
+} catch (e) {
+  console.log(e);
+}
