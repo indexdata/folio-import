d5ae45523b1068d5b92bed2dbe2e9cccb6b85a03
Charles Ledvina <charles@folio-bastion.folio-us-east-1-1.hosted-folio.indexdata.com>
2024-04-29 19:44:57 +0000

    New script for upserting globs of files.

---
+++ run_upsert_jsonl.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+EP=$1
+if [ ! $2 ] 
+then
+	echo "Usage: $0 <endpoint> <files>"
+	exit
+fi
+
+for f in ${@:2}
+do
+	LNAME=$(echo $f | sed -E 's/^.*\///')	
+	echo "View info at log/${LNAME}.info"
+	node loadJSONL.js $EP $f > log/$LNAME.info &
+done
