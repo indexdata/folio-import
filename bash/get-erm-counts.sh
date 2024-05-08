61cdfbb7bec6d2ec8faf64d04302ebe2cab69d36
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2024-05-08 16:10:16 -0500

    New bash script for getting erm counts.

---
+++ bash/get-erm-counts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TMP='./.okapi'
+OKAPI=`cat ${TMP}/url`
+TOKEN=`cat ${TMP}/token | sed 's/.$//'`
+
+EPS='erm/sas?stats=true erm/entitlements?stats=true notes?query=domain==agreements'
+
+for EP in $EPS; do
+	URL="${OKAPI}/${EP}"
+	C=`curl --http1.1 -w '\n' -s $URL -H "x-okapi-token: ${TOKEN}" | grep -o 'totalRecords": *[0-9]*' | grep -o '[0-9]*'`
+	echo "$EP: $C"
+done
