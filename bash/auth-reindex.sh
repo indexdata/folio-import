ba66ff9f2a2e10895c468da5143467767d3cde5b
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2024-10-25 11:31:09 -0500

    New script for reindexing authority

---
+++ bash/auth-reindex.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TMP='./.okapi'
+OKAPI=`cat ${TMP}/url`
+TOKEN=`cat ${TMP}/token`
+
+curl -w '\n' --http1.1 "${OKAPI}/search/index/inventory/reindex" -H 'content-type: application/json' -H "x-okapi-token: ${TOKEN}" -d '{"recreateIndex":true,"resourceName":"authority"}'
