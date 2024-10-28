81edf8402c906d226e25ebfe4b9457997f001feb
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2024-10-28 16:52:37 -0500

    Fetch 100 rows.

--- etc/pem/ITEM.sql
+++ etc/pem/ITEM.sql
@@ -4,45 +4,45 @@ set feedback off
 set termout off
 
 spool csv/item.csv
-select * from ITEM FETCH FIRST 10 ROWS ONLY;
+select * from ITEM FETCH FIRST 100 ROWS ONLY;
 spool off
 
 spool csv/item_barcode.csv
-select * from ITEM_BARCODE FETCH FIRST 10 ROWS ONLY;
+select * from ITEM_BARCODE FETCH FIRST 100 ROWS ONLY;
 spool off
 
 spool csv/bib_item.csv
-select * from BIB_ITEM FETCH FIRST 10 ROWS ONLY;
+select * from BIB_ITEM FETCH FIRST 100 ROWS ONLY;
 spool off
 
 spool csv/mfhd_item.csv
-select * from MFHD_ITEM FETCH FIRST 10 ROWS ONLY;
+select * from MFHD_ITEM FETCH FIRST 100 ROWS ONLY;
 spool off
 
 spool csv/item_note.csv
-select * from ITEM_NOTE FETCH FIRST 10 ROWS ONLY;
+select * from ITEM_NOTE FETCH FIRST 100 ROWS ONLY;
 spool off
 
 spool csv/item_status.csv
-select * from ITEM_STATUS FETCH FIRST 10 ROWS ONLY;
+select * from ITEM_STATUS FETCH FIRST 100 ROWS ONLY;
 spool off
 
 spool csv/item_status_type.csv
-select * from ITEM_STATUS_TYPE --FETCH FIRST 10 ROWS ONLY;
+select * from ITEM_STATUS_TYPE --FETCH FIRST 100 ROWS ONLY;
 spool off
 
 spool csv/item_type.csv
-select * from ITEM_TYPE --FETCH FIRST 10 ROWS ONLY;
+select * from ITEM_TYPE --FETCH FIRST 100 ROWS ONLY;
 spool off
 
 spool csv/item_type_mapping.csv
-select * from ITEM_TYPE_MAPPING FETCH FIRST 10 ROWS ONLY;
+select * from ITEM_TYPE_MAPPING FETCH FIRST 100 ROWS ONLY;
 spool off
 
 spool csv/location.csv
-select * from LOCATION --FETCH FIRST 10 ROWS ONLY;
+select * from LOCATION --FETCH FIRST 100 ROWS ONLY;
 spool off
 
 spool csv/mfhd_item.csv
-select * from MFHD_ITEM FETCH FIRST 10 ROWS ONLY;
+select * from MFHD_ITEM FETCH FIRST 100 ROWS ONLY;
 spool off
