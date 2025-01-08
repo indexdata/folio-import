a8e110424669031669d122b0554bdd128cf04c6e
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2025-01-08 15:25:10 -0600

    Finish mapping Winterthur users.

--- patrons2/usersWint.js
+++ patrons2/usersWint.js
@@ -1,6 +1,6 @@
 import { parseMarc, getSubs, getSubsHash } from '../js-marc/js-marc.mjs';
 import fs from 'fs';
-import path from 'path';
+import path, { basename } from 'path';
 import { v5 as uuid } from 'uuid';
 
 let refDir = process.argv[2];
@@ -33,16 +33,35 @@ const groups = {
   "YU": "staff"
 }
 
+const files = {
+  users: 1,
+  perms: 1,
+  rprefs: 1
+}
+
 const dform = (date) => {
   let out;
   out = date.replace(/^(....)(..)(..).*/, '$1-$2-$3');
   return out;
 }
 
+const writeOut = (path, data) => {
+  let out = JSON.stringify(data) + '\n';
+  fs.writeFileSync(path, out, { flag: 'a' });
+}
+
 try {
   if (!rawFile) throw(`Usage: node usersWint.js <ref_dir> <patron_mrc_file>`);
   const fileStream = fs.createReadStream(rawFile, { encoding: 'utf8' });
   refDir = refDir.replace(/\/$/, '');
+  let fn = path.basename(rawFile, '.mrc');
+  let dir = path.dirname(rawFile);
+
+  for (let k in files) {
+    files[k] = dir + '/' + fn + '-' + k + '.jsonl';
+    if (fs.existsSync(files[k])) fs.unlinkSync(files[k]);
+  }
+  // throw(files);
 
   // open ref files
   let rfiles = fs.readdirSync(refDir);
@@ -68,6 +87,9 @@ try {
   let start = new Date().valueOf();
   const ttl = {
     count: 0,
+    users: 0,
+    perms: 0,
+    prefs: 0,
     errors: 0
   }
   let leftOvers = '';
@@ -96,6 +118,8 @@ try {
       }
       let f = marc.fields;
       let ctrl = (f['001'])[0];
+      let f15 = f['015'];
+      let bc = (f15) ? getSubs(f15[0], 'a') : ctrl;
       let f30 = f['030'];
       let gcode = (f30) ? getSubs(f30[0], 'a') : '';
       let gname = groups[gcode] || 'student';
@@ -105,21 +129,79 @@ try {
       let exDate = (f42) ? getSubs(f42[0], 'b') : '';
       let f100 = f['100'];
       let name = (f100) ? getSubs(f100[0], 'a') : '';
+      let f271 = f['271'];
+      let email = (f271) ? getSubs(f271[0], 'a') : '';
+      let f270 = f['270'];
+      let phone = (f270) ? getSubs(f270[0], 'k') : '';
       let u = {
         id: uuid(ctrl, ns),
         username: ctrl,
         patronGroup: groupId,
+        active: true,
         personal: {}
       };
+      if (bc) {
+        u.barcode = bc;
+      }
       if (name) {
         let n = name.match(/^(.+?)(, (.+?)( (.+))?)?$/);
         u.personal.lastName = n[1];
         if (n[3]) u.personal.firstName = n[3];
         if (n[5]) u.personal.middleName = n[5];
       }
+      if (f270) {
+        f270.forEach(f => {
+          let a = getSubsHash(f, true);
+          if (a.a) {
+            if (!u.personal.addresses) u.personal.addresses = [];
+            let o = {
+              addressLine1: a.a
+            }
+            if (a.b) o.city = a.b;
+            if (a.d) o.region = a.d;
+            if (a.e) o.postalCode = a.e;
+            o.addressTypeId = refData.addressTypes.Home;
+            u.personal.addresses.push(o);
+          }
+        });
+      }
+      let f301 = f['301'];
+      let aff = (f301) ? getSubs(f301[0], 'ad') : '';
+      if (aff) {
+        u.customFields = { affiliation: aff };
+      }
+      if (email) u.personal.email = email;
+      if (phone) u.personal.phone = phone;
       if (enDate) u.enrollmentDate = dform(enDate);
       if (exDate) u.expirationDate = dform(exDate);
-      console.log(u);
+      if (process.env.DEBUG) console.log(JSON.stringify(u, null, 2));
+      if (u.personal.lastName, u.username) {
+        writeOut(files.users, u);
+        ttl.users++;
+      
+        let rpo = {
+          userId: u.id,
+          fulfillment: "Hold Shelf",
+          holdShelf: true,
+          delivery: false,
+          id: uuid(u.id + 'rp', ns),
+        };
+        writeOut(files.rprefs, rpo);
+        ttl.prefs++;
+        if (process.env.DEBUG) console.log(rpo);
+
+        let pu = {
+          userId: u.id,
+          permissions: [],
+          id: uuid(u.id + 'pu', ns)
+        };
+        writeOut(files.perms, pu);
+        ttl.perms++;
+        if (process.env.DEBUG) console.log(pu);
+      } else {
+        console.log(`ERROR Can't create user for ${ctrl}`);
+        ttl.errors++;
+      }
     }
   });
   fileStream.on('close', () => {
