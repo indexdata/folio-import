3fe03605aae07f49148420fa454c48c72d4c2f0b
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2025-02-06 11:16:49 -0600

    Add description and functions to map.

--- util/parse-map.js
+++ util/parse-map.js
@@ -5,17 +5,49 @@ for (let tag in mr) {
 	tags.push(tag);
 }
 let lines = [];
+let h = 'MARC tag\tSubfields\tFOLIO field\tDescription\tProcessing Function';
+lines.push(h);
 tags.sort().forEach(t => {
 	let rules = mr[t];
 	rules.forEach(rule => {
 		if (rule.entity) {
 			rule.entity.forEach(e => {
 				let sf = (e.subfield) ? e.subfield.join('') : ''; 
-				lines.push(`${t}\t${sf}\t${e.target}`);		
+				let func = '';
+				if (e.rules) {
+					e.rules.forEach(r => {
+						let funcs = [];
+						if (r.conditions) {
+							r.conditions.forEach(c => {
+								funcs.push(JSON.stringify(c));
+							});
+						}
+						func = funcs.join('; ');
+					}); 
+				}
+				let targ = e.target;
+				let desc = e.description;
+				let line = `${t}\t${sf}\t${targ}\t${desc}\t${func}`;		
+				lines.push(line);
 			});
 		} else {
 			let sf = (rule.subfield) ? rule.subfield.join('') : '';
-			lines.push(`${t}\t${sf}\t${rule.target}`);		
+			let func = '';
+			if (rule.rules) {
+				rule.rules.forEach(r => {
+					let funcs = [];
+					if (r.conditions) {
+						r.conditions.forEach(c => {
+							funcs.push(JSON.stringify(c));
+						});
+					}
+					func = funcs.join('; ');
+				}); 
+			}
+			let targ = rule.target;
+			let desc = rule.description; 
+			let line = `${t}\t${sf}\t${targ}\t${desc}\t${func}`;		
+			lines.push(line);
 		}
 	});
 });
