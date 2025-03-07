fd3658f0b3349e46a03abca8a10979e246d0d089
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2025-03-07 14:04:00 -0600

    New script for converting text to marc for STC authorities.

---
+++ inventory2/text2marc-stc.js
@@ -0,0 +1,64 @@
+/*
+    This script will take authority records from STC's Dump.txt file and output raw MARC21 records.
+*/
+
+import { txt2raw } from '../js-marc/js-marc.mjs';
+import fs from 'fs';
+import path from 'path';
+import readline from 'readline';
+
+const inFile = process.argv[2];
+const dbug = process.env.DEBUG;
+
+(async () => {
+    try {
+        if (!fs.existsSync(inFile)) throw('Usage: node text2marc-stc.js <text_file>');
+
+        let dir = path.dirname(inFile);
+        let outPath = `${dir}/auth.mrc`;
+        if (fs.existsSync(outPath)) fs.unlinkSync(outPath);
+        
+        let fileStream = fs.createReadStream(inFile);
+        let rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+
+        let lc = 0;
+        let rc = 0;
+        let data = '';
+        for await (let l of rl) {
+            lc++;
+            if (l.match(/^\*\*\* DOC/) && lc > 1) {
+                let raw = txt2raw(data);
+                fs.writeFileSync(outPath, raw, { flag: 'a' })
+                data = '';
+                rc++;
+                if (rc % 10000 === 0) {
+                    console.log('Records created:', rc)
+                }
+            } else {
+                if (!l.match(/^FORM=/)) {
+                    l = l.replace(/^\.(\d{3})\./, '$1');
+                    if (l.match(/^00/)) {
+                        l = l.replace(/^000 .+/, '00000nz  a2200000n  4500');
+                        l = l.replace(/^(00[1-9]) \|./, '$1 ');
+                    } else {
+                        l = l.replace(/(^...) \|/, '$1   |');
+                        l = l.replace(/\|(.)/g, ' $$$1 ');
+                    }
+                    data += l + '\n';
+                }
+            }
+        }
+        if (data) {
+            let raw = txt2raw(data);
+            fs.writeFileSync(outPath, raw, { flag: 'a' })
+            rc++;
+        }
+        console.log('Done!');
+        console.log('Total Records created:', rc);
+    } catch (e) {
+        console.log(e)
+    }
+})()
