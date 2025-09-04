3ba880d31636f40f2e205ca40d7940f184c6400a
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2025-09-04 14:22:21 -0500

    Delete bound-withs and relationships (if exist)

--- deleteInventory.js
+++ deleteInventory.js
@@ -5,7 +5,6 @@ const { getAuthToken } = require('./lib/login');
 let ten = argv._[0];
 let saveDir = argv.s;
 let instId = argv.i || '*';
-const endPoint = 'search/instances?expandAll=true&query=id==*&limit=10';
 let dbug = process.env.DEBUG;
 let tval = new Date().valueOf();
 
@@ -18,18 +17,18 @@ const sfiles = {
 
 const out = {};
 
+const sep = '----------------------------------------------------------------------------------------------';
+
 (async () => {
   try {
     if (!ten) {
-      throw new Error('Usage: node deleteInventory.js <tenant> [ <save_dir> ]');
+      throw new Error('Usage: node deleteInventory.js <tenant> [ options: -s <save_dir>, -i <instance_id> ]');
     }
 
     let config = await getAuthToken(superagent);
     if (ten !== config.tenant) {
       throw new Error(`Tenent "${ten}" does not match "${config.tenant}"`);
     }
-
-    let actionUrl = config.okapi + '/' + endPoint;
 
     if (saveDir) {
       saveDir = saveDir.replace(/\/$/, '');
@@ -40,6 +39,38 @@ const out = {};
       }
     }
     // console.log(sfiles); return;
+
+    const delBound = async (url) => {
+      let id = url.replace(/.+\//, '');
+      let idType = (url.match(/items/)) ? 'itemId' : 'holdingsRecordId';
+      let q = `${idType}==${id}`;
+      let base = `${config.okapi}/inventory-storage/bound-with-parts`;
+      let gurl = `${base}?query=${q}`; 
+      let res = await get(gurl);
+      for (let x = 0; x < res.boundWithParts.length; x++) {
+        let r = res.boundWithParts[x];
+        let durl = `${base}/${r.id}`;
+        await del(durl);
+      }
+      let c = await del(url, true);
+      return c;
+    }
+
+    const delRel = async (url) => {
+      let id = url.replace(/.+\//, '');
+      let q = `superInstanceId==${id} OR subInstanceId==${id}`;
+      let base = `${config.okapi}/instance-storage/instance-relationships`;
+      let gurl = `${base}?query=${q}`; 
+      let res = await get(gurl);
+      let c = 0;
+      for (let x = 0; x < res.instanceRelationships.length; x++) {
+        let r = res.instanceRelationships[x];
+        let durl = `${base}/${r.id}`;
+        await del(durl);
+        c++;
+      }
+      return c;
+    }
 
     const get = async (url) => {
       try {
@@ -54,7 +85,7 @@ const out = {};
       }
     }
 
-    const del = async (url) => {
+    const del = async (url, noRetry) => {
       try {
         console.log(`DELETE ${url}`);
         let res = await superagent
@@ -62,8 +93,17 @@ const out = {};
           .set('x-okapi-token', config.token)
         return res;
       } catch (e) {
-        console.log(e);
-        return -1;
+        let msg = (e.response && e.response.text) ? e.response.text : e;
+        if (!noRetry && msg && msg.match(/bound_with/)) {
+          let c = await delBound(url);
+          return c;
+        } else if (!noRetry && msg && msg.match(/instance_relationship/)) {
+          let c = await delRel(url);
+          return c;
+        } else {
+          console.log(msg);
+          return -1;
+        }
       }
     }
 
@@ -103,6 +143,7 @@ const out = {};
     };
     let totRecs = 1000000;
     let perPage = 10; 
+    console.log(sep);
     while (totRecs > 0) {
       let url = `${config.okapi}/search/instances?expandAll=true&query=id==${instId} sortby title&limit=${perPage}`;
       let res = await get(url);
@@ -110,6 +151,7 @@ const out = {};
       totRecs = res.totalRecords;
       if (totRecs === 0) {
         console.log(`No instances found`);
+        console.log(sep);
         continue;
       }
       for (let x = 0; x < recs.length; x++) {
@@ -122,11 +164,14 @@ const out = {};
         }
         if (hc > -1) {
           ttl.holdings += hc;
-          await del(`${config.okapi}/instance-storage/instances/${rec.id}`);
-          ttl.instances++;
-          await del(`${config.okapi}/source-storage/records/${rec.id}?idType=INSTANCE`);
-          ttl.srs++;
+          let bc = await del(`${config.okapi}/instance-storage/instances/${rec.id}`);
+          if (bc !== -1) {
+            ttl.instances++;
+            let sc = await del(`${config.okapi}/source-storage/records/${rec.id}?idType=INSTANCE`);
+            if (sc !== -1) ttl.srs++;
+          }
         }
+        console.log(sep);
       }
       totRecs = 0;
     }
