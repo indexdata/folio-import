1b7216c2745874cc0a14c5412b1d80839041d491
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2025-12-02 13:51:49 -0600

    Add pattern maker

--- acquisitions/nlsSerials.js
+++ acquisitions/nlsSerials.js
@@ -21,7 +21,8 @@ const tstr = 'KB Stående order';
 let inFile = process.argv[2];
 
 const files = {
-  s: 'serials'
+  s: 'serials',
+  r: 'ruleSetRaw'
 };
 
 const ifiles = {
@@ -32,9 +33,28 @@ const ifiles = {
 let newDate = new Date();
 let start = newDate.valueOf();
 
+const months = [ '', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december' ];
+
 const writeOut = (fileName, data) => {
   let line = JSON.stringify(data) + "\n";
   fs.writeFileSync(fileName, line, { flag: 'a' });
+}
+
+const makeRules = (ptype, idate, interval) => {
+  let reps = interval.iov;
+  let out = [];
+  for (let x = 0; x < reps; x++) {
+    let o = { pattern: {}, patternType: ptype };
+    o.ordinal = 1;
+    o.pattern.day = idate.d;
+    if (ptype === 'year_date') {
+      if (x > 0) idate.mv += interval.count;
+      o.pattern.month = { value: months[idate.mv] };
+    }
+    out.push(o);
+  }
+  
+  return out;
 }
 
 (async () => {
@@ -85,6 +105,22 @@ const writeOut = (fileName, data) => {
     lines.forEach(r => {
       // console.log(r);
       let rk = r.Z08_REC_KEY;
+      let tu = (r.Z08_VOLUME_PERIOD_TYPE === 'Y') ? 'year' : 'month';
+      let per = r.Z08_VOLUME_PERIOD;
+      let ipv = r.Z08_NO_ISSUE_PER_VOLUME;
+      let interval = { type: r.Z08_INTERVAL_TYPE, count: parseInt(r.Z08_INTERVAL_COUNT, 10), iov: parseInt(r.Z08_NO_ISSUE_OVER_VOLUME) };
+      let idate = {};
+      let dateStr = r.Z08_ISSUE_DATE;
+      if (dateStr) {
+        idate.y = dateStr.substring(0, 4);
+        idate.m = dateStr.substring(4, 6);
+        idate.d = dateStr.substring(6, 8);
+      }
+      for (let k in idate) {
+        idate[k] = idate[k].replace(/^0/, '');
+        idate[k + 'v'] = parseInt(idate[k], 10);
+      }
+      let ptype = (tu === 'year') ? 'year_date' : 'month_date';
       let polId = d.a[rk];
       if (polId) {
         let t = d.t[polId];
@@ -99,6 +135,20 @@ const writeOut = (fileName, data) => {
         }
         writeOut(files.s, so);
         ttl.serials++;
+
+        let rs = {
+          owner: { id: so.id },
+          recurrence: {
+            timeUnit: { value: tu }
+          },
+          period: per,
+          issues: ipv,
+        }
+
+        rs.rules = makeRules(ptype, idate, interval);
+
+        writeOut(files.r, rs);
+        console.log(JSON.stringify(rs, null, 2))
       }
       else {
         console.log(`ERROR PO-line not found for ${rk}`);
