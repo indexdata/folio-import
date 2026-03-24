33b3dbcedebcb2f060d5d70eb4a5f61c0e62a152
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2026-03-24 11:56:11 -0500

    Circulation notes remapping

--- inventory2/holdingsItems-ba.js
+++ inventory2/holdingsItems-ba.js
@@ -100,6 +100,17 @@ const makeItemNote = function (text, type, staffOnly) {
   return out;
 }
 
+const makeCircNote = function (text, type, itemId) {
+  let out = {
+    note: text,
+    noteType: type,
+    date: new Date().toISOString().replace(/T.+/, ''),
+    id: uuid(itemId + type + text, ns),
+    staffOnly: true
+  };
+  return out;
+}
+
 try {
   if (!mfhdFile) { throw "Usage: node holdingsItems-ba.js <conf_file> <mfhd_jsonl_file>" }
   let confDir = path.dirname(confFile);
@@ -222,7 +233,7 @@ try {
       let ih = {};
       r.subfields.forEach(s => {
         let code = Object.keys(s)[0]
-        if (code.match(/[jqfzmep_]/)) {
+        if (code.match(/[jqfzmep_ru]/)) {
           if (!ih[code]) ih[code] = [];
           ih[code].push(s[code]);
         } else {
@@ -325,6 +336,7 @@ try {
       if (ih.n) desc.push(ih.n);
       if (ih.o) desc.push(ih.o);
       if (desc[0]) i.descriptionOfPieces = desc.join('; ');
+      if (ih.x) i.numberOfPieces = ih.x;
       for (let k in inotes) {
         if (ih[k]) {
           let tstr = inotes[k];
@@ -337,16 +349,15 @@ try {
           }
         }
       }
-      if (ih.q) {
-        i.circulationNotes = [];
-        let t = (ih.r && ih.r.match(/discharge/)) ? 'Check in' : 'Check out';
-        ih.q.forEach(d => {
-          let o = {
-            note: d,
-            noteType: t,
-            date: new Date().toISOString().replace(/T.+/, ''),
-            id: uuid(iid + t + d, ns)
-          };
+      if (ih.r) {
+        ih.r.forEach(d => {
+          let o = makeCircNote(d, 'Check in', iid);
+          i.circulationNotes.push(o);
+        });
+      }
+      if (ih.u) {
+        ih.u.forEach(d => {
+          let o = makeCircNote(d, 'Check out', iid);
           i.circulationNotes.push(o);
         });
       }
@@ -629,86 +640,6 @@ try {
       ttl.holdingsErr++;
     }
     
-  }
-
-  /*
-    This is the item creation section
-  */
-  
-  
-  let items = {};
-  for (let x in items) {
-    let r = items[x];
-    let iid = r.ITEM_ID;
-    let id = uuid(iid, ns);
-    let hrid = iprefix + iid;
-    let m = mmap[iid];
-    let vhid = (m) ? m.MFHD_ID : '';
-    let hid = hseen[vhid];
-    if (hid) {
-      let i = {
-        _version: 1,
-        id: id,
-        hrid: hrid,
-        holdingsRecordId: hid,
-        formerIds: [ iid ],
-        status: { name: 'Available' },
-        notes: []
-      };
-      let bc = m.BARCODE || 'TEMP' + iid;
-      if (!bcseen[bc]) {
-        i.barcode = bc;
-        if (!m.BARCODE) {
-          let o = {
-            note: 'Barcode added during migrations.',
-            itemNoteTypeId: refData.itemNoteTypes.Note,
-            staffOnly: true
-          }
-          i.notes.push(o);
-        }
-        bcseen[bc] = iid;
-      } else {
-        i.barcode = 'TEMP' + iid;
-        let o = {
-          note: `Barcode ${bc} already used by pi${bcseen[bc]}. Using ITEM_ID instead.`,
-          itemNoteTypeId: refData.itemNoteTypes.Note,
-          staffOnly: true
-        }
-        i.notes.push(o);
-        console.log(`WARN barcode ${bc} already used by ITEM_ID ${bcseen[bc]}`);
-      }
-      if (m.ITEM_ENUM) i.volume = m.ITEM_ENUM;
-      if (m.CHRON) i.chronology = m.CHRON;
-      if (m.YEAR) i.yearCaption = [ m.YEAR ];
-      if (r.COPY_NUMBER) i.copyNumber = r.COPY_NUMBER;
-      if (r.PIECES) i.numberOfPieces = r.PIECES;
-      let nts = m.NOTES || [];
-      nts.forEach(n => {
-        let o = {
-          note: n,
-          itemNoteTypeId: refData.itemNoteTypes.Note
-        };
-        i.notes.push(o);
-      });
-      if (m.STATUS) i.status.name = m.STATUS;
-      let vtype = r.ITEM_TYPE_ID;
-      i.materialTypeId = tsvMap.mtypes[vtype];
-      i.permanentLoanTypeId = refData.loantypes.Standard;
-      let tloc = locMap[r.TEMP_LOCATION];
-      if (tloc) i.temporaryLocationId = tloc;
-      if (i.materialTypeId) {
-        if (i.permanentLoanTypeId) {
-          writeOut(outs.items, i);
-        } else {
-          console.log(`ERROR loantype not found for ${iid}`);
-          ttl.itemErr++;
-        }
-      } else {
-        console.log(`ERROR material type "${vtype}" not found for ${iid}`);
-        ttl.itemErr++;
-      }
-      ttl.items++;
-    }
   }
 
   let occur = {};
