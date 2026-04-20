ad6c08d44dc5d32f6b3e5b7ed1acc90750bdebd1
Charles Ledvina <charles@Charless-MacBook-Pro.local>
2026-04-20 15:50:27 -0500

    finish purchase orders mapping for MSM

--- acquisitions/msmOrders.js
+++ acquisitions/msmOrders.js
@@ -11,8 +11,8 @@ const filter = process.argv[5];
 const ns = 'eab3e237-5dc1-4a4b-b4da-5ab5acc3d0ee';
 
 const files = {
-  co: 'composite-orders.jsonl',
   ord: 'purchase-orders.jsonl',
+  open: 'purchase-orders-open.jsonl',
   pol: 'pol-lines.jsonl'
 };
 
@@ -20,6 +20,28 @@ const rfiles = {
   organizations: 'organizations.json',
   funds: 'funds.json',
   acquisitionMethods: 'acquisition-methods.json'
+};
+
+const nfields = [
+  "varField_ft_f",
+  "varField_ft_n",
+  "varField_ft_n_2",
+  "varField_ft_s",
+  "varField_ft_v",
+  "varField_ft_x",
+  "varField_ft_z",
+  "varField_ft_z_2",
+  "varField_ft_z_3"
+]
+
+const otypeMap = {
+  "a": "One-Time",
+	"n": "One-Time",
+	"o": "Ongoing",
+	"r": "One-Time",
+	"s": "Ongoing",
+	"t": "One-Time",
+	"v": "One-Time"
 };
 
 (async () => {
@@ -51,7 +73,7 @@ const rfiles = {
         if (c && v) refData[f][c] = v;
       });
     }
-    // throw(refData);
+    if (process.env.DEBUG === 'ref') throw(refData);
 
     console.log(`INFO Creating instance map...`);
     let instMap = {};
@@ -94,12 +116,71 @@ const rfiles = {
     // throw(inRecs); 
 
     const seen = {};
-    const ttl = { count: 0, purchaseOrders: 0, Pols: 0};
+    const ttl = { count: 0, orders : 0, ordersOpen: 0, poLines: 0, errors: 0 };
     for (let x = 0; x < inRecs.length; x++) {
       let r = inRecs[x];
       if (filter && !r[filter]) continue;
       ttl.count++;
-      if (process.env.DEBUG) console.log(r);
+      if (process.env.DEBUG === 'r') console.log(r);
+      let poNum = r.id;
+      let ven = r.VENDOR.trim();
+      let venId = refData.organizations[ven];
+      let otype = r.ORD_TYPE;
+      let otypeStr = (otype.match(/^[os]$/)) ? "Ongoing" : "One-Time";
+      let copies = r.COPIES;
+      let st = r.STATUS;
+      let statStr = (st.match(/[1q]/)) ? 'Pending' : (st.match(/[az]/)) ? 'Closed' : 'Open';
+
+      if (seen[poNum]) {
+        console.log(`ERROR "${poNum}" already used`);
+        ttl.errors++;
+        continue;
+      }
+      if (!poNum.match(/\d/)) {
+        console.log(`ERROR "${poNum}" is not a valid poNumber`);
+        ttl.errors++;
+        continue;
+      }
+      if (!venId) {
+        console.log(`ERROR vendor not found for "${ven}"`)
+        ttl.errors++;
+        continue;
+      }
+
+      let id = uuid(poNum, ns);
+      let odate;
+      try {
+        odate = new Date(r.ODATE).toISOString().substring(0, 10);
+      } catch (e) {
+        console.log(`WARN "${r.ODATE}" is not a valid date`);
+      }
+      
+      let o = {
+        id: id,
+        poNumber: poNum,
+        vendor: venId,
+        orderType: otypeStr,
+        workflowStatus: statStr,
+        notes: []
+      }
+      if (odate) o.dateOrdered = odate;
+      // o.isSubscription = (r.ORD_TYPE === 's') ? true : false;
+      nfields.forEach(f => {
+        let n = r[f];
+        if (n) o.notes.push(n);
+      });
+      // if (copies) o.totalItems = parseInt(copies, 10);
+
+      if (process.env.DEBUG === 'o') console.log(o);
+
+      if (o.workflowStatus === 'Open') {
+        writeTo(files.open, o);
+        o.workflowStatus = 'Pending';
+        ttl.ordersOpen++;
+      }
+      writeTo(files.ord, o); 
+      seen[o.poNum] = 1;
+      ttl.orders++;
     }
 
     console.log('Finished!');
